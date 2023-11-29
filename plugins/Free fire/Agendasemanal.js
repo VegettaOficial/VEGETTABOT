c378959a26b6e23327510f46ffb8a012ef1ea6c2
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2023-11-29 14:59:21 -0500

    Update Agendasemanal.js

--- plugins/Free fire/Agendasemanal.js
+++ plugins/Free fire/Agendasemanal.js
@@ -2,14 +2,22 @@ let handler = async (m, { isPrems, conn }) => {
 let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
 if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
 
-let img = 'https://telegra.ph/file/b532203bcfb979554a0db.jpg' 
-let texto = `📕 ¡LA #AGENDASEMANAL ESTÁ AQUÍ! 📕
+let img = 'https://telegra.ph/file/f296b3635a5ae306f9bf7.jpg' 
+let texto = `
 
-⚠ PELIGRO: Una amenaza monstruosa está suelta en FF. 🔥  
 
-Crece tu colección #AtaqueMonstruoso con la ⏫ velocidad de disparo y ➕ precisión mejorada de la SCAR Ataque Monstruoso, 🕺 celebra el Booyah! con un EMOTE bestial y 🚗 recorre Bermuda al volante del Auto Deportivo Monstruo Transformador.
 
-¡Vuélvete peligrosamente poderoso y vístete de gloria con el aspecto especial de la #FFWS2023!
+📕 ¡LA #AGENDASEMANAL ESTÁ AQUÍ! 📕
+
+🛑 Alerta: 🌜 ¡LUNA SANGRIENTA ESTÁ DE VUELTA! Aparta la fecha y ve por los 4 aspectos de la colección en la Ruleta de Tokens.
+
+🪐 Además, emprende una Odisea Galáctica con la NUEVA TEMPORADA del Pase BOOYAH! y revoluciona tu arsenal con la velocidad de la M1887 y la katana Poder Esmeralda. 🟩
+
+💥 ¡Entra ahora! Poder y recompensas te esperan.
+
+
+
+
 `
 
 const fkontak = {
