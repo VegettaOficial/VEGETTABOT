a9f3e7e558912e7bc4383032713b9ade1a72efb3
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2023-11-30 15:17:43 -0500

    Update Prueba2.js

--- plugins/Prueba2.js
+++ plugins/Prueba2.js
@@ -1,32 +1,59 @@
+import fetch from 'node-fetch'
 import axios from 'axios'
-let handler = async (m, {conn, args}) => {
-
-if (!args[0]) return conn.reply(m.chat, `*🚩 Escribe la URL de un video de Instagram que deseas descargar.*`, m, adReply)
-await m.react('🕓')
-let url = `https://vihangayt.me/download/instagram?url=${encodeURIComponent(args[0])}`
-  
+import instagramGetUrl from 'instagram-url-direct'
+import {instagram} from '@xct007/frieren-scraper'
+import {instagramdl} from '@bochilteam/scraper'
+ 
+var handler = async (m, {conn, args, command, usedPrefix}) => {
+ 
+if (!args[0]) throw `*⚠️ INGRESE UN ENLACE DE INSTAGRAM*\n\n❕ EJEMPLO:\n*${usedPrefix + command}* https://www.instagram.com/reel/CuqAzGRAbZa/?igshid=MzRlODBiNWFlZA==`
+conn.reply(m.chat, `*🧃 DESCARGANDO SU PEDIDO, POR FAVOR SEA PACIENTE*`, fkontak, m)
 try {
-const response = await axios.get(url)
-if (!response.data.status) {
-throw new Error(`Error al obtener datos`)
+const apiUrll = `https://api.betabotz.org/api/download/igdowloader?url=${encodeURIComponent(args[0])}&apikey=bot-secx3`
+const responsel = await axios.get(apiUrll)
+const resultl = responsel.data
+for (const item of resultl.message) {
+const shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${item.thumbnail}`)).text()
+const tXXxt = `🍧 *Url:* ${shortUrRRl}`.trim()
+conn.sendFile(m.chat, item._url, null, tXXxt, fkontak, m)
+await new Promise((resolve) => setTimeout(resolve, 10000))
+} 
+} catch { 
+try { 
+const datTa = await instagram.v1(args[0])
+for (const urRRl of datTa) {
+const shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
+const tXXxt = `🍧 *Url:* ${shortUrRRl}`.trim()
+conn.sendFile(m.chat, urRRl.url, 'error.mp4', tXXxt, fkontak, m)
+await new Promise((resolve) => setTimeout(resolve, 10000))
 }
-const data = response.data.data
-if (data && data.data && data.data.length > 0) {
-const videoURL = data.data[0].url
+} catch {
+try {
+const resultss = await instagramGetUrl(args[0]).url_list[0]
+const shortUrl2 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
+const txt2 = `🍧 *Url:* ${shortUrl2}`.trim()
+await conn.sendFile(m.chat, resultss, 'error.mp4', txt2, m)
+} catch {
+try {
+const resultssss = await instagramdl(args[0])
+const shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
+const txt4 = `🍧 *Url:* ${shortUrl3}`.trim()
+for (const {url} of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt4, m)
+} catch {
+try {
+const human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`)
+const json = await human.json()
+const videoig = json.result
+const shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
+const txt1 = `🍧 *Url:* ${shortUrl1}`.trim()
+await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m)
+} catch {
+throw `*⚠️ OCURRIÓ UN FALLO, VUELVE A INTENTAR*`
+}}}}} 
 
-await conn.sendFile(m.chat, videoURL, 'instagram_reel.mp4', '', estilo)
-await m.react('✅')
-} else {
-await conn.reply(m.chat, 'No puedo encontrar el vídeo de Instagram.', m, adReply).then(_ => m.react('✖️'))
 }
-} catch (error) {
-console.error(error)
-conn.reply(m.chat, '*☓ Ocurrió un error inesperado*', m, adReply).then(_ => m.react('✖️'))
-}
-}
-handler.help = ['instagram'].map(v => v + ' <url ig>')
-handler.tags = ['downloader'];
-handler.command = /^(instagramdl|instagram|igdl|ig)$/i;
-handler.star = 2
-handler.register = false
+handler.help = ['ig']
+handler.tags = ['descargas']
+handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i
+ 
 export default handler
