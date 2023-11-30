7067ccf0122132ed144aeb1fafc7186aaccc3a8d
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2023-11-30 00:22:27 -0500

    Update prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -1,25 +1,25 @@
-import fs from 'fs'
-import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `${mg}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀 𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈 𝙋𝘼𝙍𝘼 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙍 𝙎𝙐 𝙑𝙄𝘿𝙀𝙊 𝙊 𝙄𝙈𝘼𝙂𝙀𝙉\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} https://www.instagram.com/tv/Cd8U99IloVA/?igshid=YmMyMTA2M2Y=*\n\n𝙀𝙉𝙏𝙀𝙍 𝘼 𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈 𝙇𝙄𝙉𝙆 𝙏𝙊 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿 𝙔𝙊𝙐𝙍 𝙑𝙄𝘿𝙀𝙊 𝙊𝙍 𝙄𝙈𝘼𝙂𝙀\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} https://www.instagram.com/p/CCoI4DQBGVQ/?igshid=YmMyMTA2M2Y=*`
-const results = await instagramdl(args[0])
-.catch(async _ => await instagramdlv2(args[0]))
-.catch(async _ => await instagramdlv3(args[0]))
-.catch(async _ => await instagramdlv4(args[0]))
-for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `✨ *ENLACE | URL:* ${url}`, m)
-  
- let info = `💖 *Infórmate sobre las Novedades y recuerda tener la última versión.*\n\n💝 *Find out about what's new and remember to have the latest version.*
-  `.trim()
-  
-await conn.sendHydrated(m.chat, info, wm, null, ig, '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
-['𝙈𝙚𝙣𝙪 𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙨 🌀', '#descargasmenu'],
-['𝙈𝙚𝙣𝙪 𝘾𝙤𝙢𝙥𝙡𝙚𝙩𝙤 | 𝙁𝙪𝙡𝙡 𝙈𝙚𝙣𝙪 ✨', '.allmenu'],
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
-], m,)
-  
-} 
-handler.help = ['ig'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-handler.command =/^(instagram|ig(dl)?)$/i 
-handler.exp = 0
-export default handler
+//import {spin} from "../lib/scrape.js";
+
+let handler = async (m, {conn, args, usedPrefix, command}) => {
+  if (!args[0])
+    throw `**Este comando para descargar videos de pinterest con enlace*\n\nejemplo:\n${
+      usedPrefix + command
+    } https://id.pinterest.com/pin/27162403992537372/*`;
+  if (!args[0].match(/https:\/\/.*pinterest.com\/pin|pin.it/gi))
+    throw `*¡Enlace incorrecto! Este comando para descargar videos de pinterest con enlace*\n\nejemplo:\n${
+      usedPrefix + command
+    } https://id.pinterest.com/pin/27162403992537372/*`;
+  await spin(args[0]).then(async (res) => {
+    let pin = JSON.stringify(res);
+    let json = JSON.parse(pin);
+    if (!json.status) throw `No se puede descargar`;
+    await conn.sendButton(m.chat, json.data.url, `*Mythia Batford*`, m);
+  });
+};
+handler.help = ["pinterestvideo"].map((v) => v + " <url>");
+handler.tags = ["downloader"];
+handler.command = /^pinterestvideo$/i;
+
+handler.limit = true;
+
+export default handler;
