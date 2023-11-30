84f9eb1b49c5d80c491825a967ec5f6445a5c596
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2023-11-30 14:46:47 -0500

    Update Prueba2.js

--- plugins/Prueba2.js
+++ plugins/Prueba2.js
@@ -1,75 +1,58 @@
-import fetch from 'node-fetch';
+ 
+import fetch from 'node-fetch'
 import axios from 'axios';
-import instagramGetUrl from 'instagram-url-direct';
-import {instagram} from '@xct007/frieren-scraper';
-import {instagramdl} from '@bochilteam/scraper';
-const handler = async (m, {conn, args, command, usedPrefix}) => {
-const fkontak = {
-        "key": {
-        "participants":"0@s.whatsapp.net",
-            "remoteJid": "status@broadcast",
-            "fromMe": false,
-            "id": "Halo"    
-        }, 
-        "message": {
-            "contactMessage": {
-                "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-            }
-        }, 
-        "participant": "0@s.whatsapp.net"
-    }
-if (!args[0]) throw `${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀 𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈 𝙋𝘼𝙍𝘼 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙍 𝙎𝙐 𝙑𝙄𝘿𝙀𝙊 𝙊 𝙄𝙈𝘼𝙂𝙀𝙉\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} https://www.instagram.com/reel/Cx-zPz-LAuV/?igshid=MTgyaXdpZ2hnbHR6NA==*`
-  const { key } = await conn.sendMessage(m.chat, {text: wait}, {quoted: fkontak});
-// await delay(1000 * 2);
-await conn.sendMessage(m.chat, {text: waitt, edit: key});
-await conn.sendMessage(m.chat, {text: waittt, edit: key});
-await conn.sendMessage(m.chat, {text: waitttt, edit: key});
-try {
-const apiUrll = `https://api.betabotz.org/api/download/igdowloader?url=${encodeURIComponent(args[0])}&apikey=bot-secx3`;
-const responsel = await axios.get(apiUrll);
-const resultl = responsel.data;
-for (const item of resultl.message) {
-const shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${item.thumbnail}`)).text();
-let tXXxt = `✨ *ENLACE | URL:* ${shortUrRRl}\n\n${wm}`.trim()  
-conn.sendFile(m.chat, item._url, null, tXXxt, m);
-await new Promise((resolve) => setTimeout(resolve, 10000));
-}} catch {    
-try {
-const datTa = await instagram.v1(args[0]);
-for (const urRRl of datTa) {
-const shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-const tXXxt = `✨ *ENLACE | URL:* ${shortUrRRl}\n\n${wm}`.trim();
-conn.sendFile(m.chat, urRRl.url, 'error.mp4', tXXxt, m);
-await new Promise((resolve) => setTimeout(resolve, 10000));
-}} catch {
-try {
-const resultss = await instagramGetUrl(args[0]).url_list[0];
-const shortUrl2 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-const txt2 = `✨ *ENLACE | URL:* ${shortUrl2}\n\n${wm}`.trim();
-await conn.sendFile(m.chat, resultss, 'error.mp4', txt2, m);
-} catch {
-try {
-const resultssss = await instagramdl(args[0]);
-const shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-const txt4 = `✨ *ENLACE | URL:* ${shortUrl3}\n\n${wm}`.trim();
-for (const {url} of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt4, m);
-} catch {
-try {
-const human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`);
-const json = await human.json();
-const videoig = json.result;
-const shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-const txt1 = `✨ *ENLACE | URL:* ${shortUrl1}\n\n${wm}`.trim();
-await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m);
-} catch {
-console.log('Error en el intento 4, sin resultados')  
-conn.sendMessage(m.chat, {text: `${lenguajeGB['smsAvisoFG']()} 𝙀𝙍𝙍𝙊𝙍 𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍 𝙑𝙐𝙀𝙇𝙑𝘼 𝘼 𝙄𝙉𝙏𝙀𝙉𝙏𝘼`, edit: key});
-handler.limit = 0
-}}}}}}
+import cheerio from 'cheerio'; 
+import fg from 'api-dylux'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `✳️ ${mssg.useCmd}\n *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
+    m.react(rwait)
+
+   let res = await igdl(args[0])
+    for (let result of res.data) {
+    conn.sendFile(m.chat, result.url, 'igdl.mp4', `✅ ${mssg.result}`, m)
+    m.react(done)
+  }
+}
 handler.help = ['instagram <link ig>']
-handler.tags = ['downloader']
-handler.command =/^(instagram|ig(dl)?)$/i
-handler.limit = 0
-handler.exp = 0
-handler.register = false
-export default handler
+handler.tags = ['dl']
+handler.command = ['ig', 'igdl', 'instagram', 'igimg', 'igvid'] 
+handler.diamond = true
+
+export default handler 
+
+
+async function igdl(url) {
+   try {
+      const response = await axios.post("https://saveig.app/api/ajaxSearch", new URLSearchParams({ q: url, t: "media", lang: "en" }).toString(), {
+         headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Origin': 'https://saveig.app/en',
+            'Referer': 'https://saveig.app/en',
+            'Referrer-Policy': 'strict-origin-when-cross-origin',
+            'User-Agent': 'PostmanRuntime/7.31.1'
+         }
+      });
+
+      const $ = cheerio.load(response.data.data);
+      const data = $('div.download-items__btn').map((i, e) => {
+         const type = $(e).find('a').attr('href').match('.jpg') ? 'image' : 'video';
+         const url = $(e).find('a').attr('href');
+         return {
+ type, 
+url
+ };
+      }).get();
+
+      return {
+         status: data.length > 0,
+         data
+      };
+   } catch (error) {
+      
+      return {
+         status: false,
+         msg: error.message
+      };
+   }
+} 
