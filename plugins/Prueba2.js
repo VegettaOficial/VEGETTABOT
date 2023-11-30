e6a4c1cee0ae19766dcb895ab52eb86c033213cc
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2023-11-30 10:49:11 -0500

    Update Prueba2.js

--- plugins/Prueba2.js
+++ plugins/Prueba2.js
@@ -3,21 +3,19 @@ import axios from "axios";
 import cheerio from "cheerio";
 import fg from "api-dylux";
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0])
-    throw `*_📌️ Uso del comando_*\n *${
-      usedPrefix + command
-    }* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`;
-  if (!args[0].match(/instagram/gi))
+if (!args[0])
+throw `*_📌️ Uso del comando_*\n *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`;
+if (!args[0].match(/instagram/gi))
 throw `❎ Asegurese que el enlace sea de Instagram`;
-await conn.sendNyanCat( m.chat, global.wait, imagen2, `Descarga de Instagram`, null, nn, m );
-  let res = await igdl(args[0]);
-  for (let result of res.data) {
-    conn.sendFile(m.chat, result.url, "igdl.mp4", ``, m);
+m.reply(`*⏰ espere un momento*`)
+let res = await igdl(args[0]);
+for (let result of res.data) {
+conn.sendFile(m.chat, result.url, "igdl.mp4", ``, m);
   }
 };
 handler.help = ["instagram *<link ig>*"];
 handler.tags = ["downloader"];
-handler.command = ["ig", "igdl", "instagram", "insta", "igvid"];
+handler.command = ["ig", "igdl", "instagram", "instaa", "igvid"];
 
 export default handler;
 
