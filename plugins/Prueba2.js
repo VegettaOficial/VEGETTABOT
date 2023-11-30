d8bb12dbae255c8d96921de3a2cb9ca126c3f601
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2023-11-30 15:16:01 -0500

    Update Prueba2.js

--- plugins/Prueba2.js
+++ plugins/Prueba2.js
@@ -1,73 +1,32 @@
-import fetch from "node-fetch";
-import axios from "axios";
-import cheerio from "cheerio";
-import fg from "api-dylux";
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0])
-    throw `*_📌️ Uso del comando_*\n *${
-      usedPrefix + command
-    }* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`;
-  if (!args[0].match(/instagram/gi))
-    throw `❎ Asegurese que el enlace sea de Instagram`;
-  await conn.sendNyanCat(
-    m.chat,
-    global.wait,
-    adnyancat,
-    addescargas,
-    null,
-    script,
-    m
-  );
-  let res = await igdl(args[0]);
-  for (let result of res.data) {
-    conn.sendFile(m.chat, result.url, "igdl.mp4", ``, m);
-  }
-};
-handler.help = ["instagram *<link ig>*"];
-handler.tags = ["downloader"];
-handler.command = ["ig", "igdl", "instagram", "igimg", "igvid"];
+import axios from 'axios'
+let handler = async (m, {conn, args}) => {
 
-export default handler;
-
-async function igdl(url) {
-  try {
-    const response = await axios.post(
-      "https://saveig.app/api/ajaxSearch",
-      new URLSearchParams({ q: url, t: "media", lang: "en" }).toString(),
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-          "Accept-Encoding": "gzip, deflate, br",
-          Origin: "https://saveig.app/en",
-          Referer: "https://saveig.app/en",
-          "Referrer-Policy": "strict-origin-when-cross-origin",
-          "User-Agent": "PostmanRuntime/7.31.1",
-        },
-      }
-    );
-
-    const $ = cheerio.load(response.data.data);
-    const data = $("div.download-items__btn")
-      .map((i, e) => {
-        const type = $(e).find("a").attr("href").match(".jpg")
-          ? "image"
-          : "video";
-        const url = $(e).find("a").attr("href");
-        return {
-          type,
-          url,
-        };
-      })
-      .get();
-
-    return {
-      status: data.length > 0,
-      data,
-    };
-  } catch (error) {
-    return {
-      status: false,
-      msg: error.message,
-    };
-  }
+if (!args[0]) return conn.reply(m.chat, `*🚩 Escribe la URL de un video de Instagram que deseas descargar.*`, m, adReply)
+await m.react('🕓')
+let url = `https://vihangayt.me/download/instagram?url=${encodeURIComponent(args[0])}`
+  
+try {
+const response = await axios.get(url)
+if (!response.data.status) {
+throw new Error(`Error al obtener datos`)
 }
+const data = response.data.data
+if (data && data.data && data.data.length > 0) {
+const videoURL = data.data[0].url
+
+await conn.sendFile(m.chat, videoURL, 'instagram_reel.mp4', '', estilo)
+await m.react('✅')
+} else {
+await conn.reply(m.chat, 'No puedo encontrar el vídeo de Instagram.', m, adReply).then(_ => m.react('✖️'))
+}
+} catch (error) {
+console.error(error)
+conn.reply(m.chat, '*☓ Ocurrió un error inesperado*', m, adReply).then(_ => m.react('✖️'))
+}
+}
+handler.help = ['instagram'].map(v => v + ' <url ig>')
+handler.tags = ['downloader'];
+handler.command = /^(instagramdl|instagram|igdl|ig)$/i;
+handler.star = 2
+handler.register = false
+export default handler
