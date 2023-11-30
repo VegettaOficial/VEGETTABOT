ec5411e0c2d101cbb5ca05dce3ac5dfda0b86c56
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2023-11-29 22:33:15 -0500

    Update propietario(a)-update.js

--- plugins/Propietario/propietario(a)-update.js
+++ plugins/Propietario/propietario(a)-update.js
@@ -1,18 +1,17 @@
-import {execSync} from "child_process";
-let handler = async (m, {conn, text}) => {
-  await m.reply(`*[ 𝐂𝐀𝐑𝐆𝐀𝐍𝐃𝐎 ]*`);
-  try {
-    if (global.conn.user.jid == conn.user.jid) {
-      let stdout = execSync("git pull" + (m.fromMe && text ? " " + text : ""));
-      await await await conn.reply(m.chat, stdout.toString(), m);
-    }
-  } catch {
-    var update = execSync("git remote set-url origin https://github.com/DIEGO-OFC/ShadowBot-MD.git && git pull");
-    await await await m.reply(update.toString());
-  }
-};
-handler.help = ["update"];
-handler.tags = ["owner"];
-handler.command = /^update|actualizar$/i;
-handler.rowner = true;
-export default handler;
+import { execSync } from 'child_process'
+let handler = async (m, { conn, text }) => {
+try {  
+if (global.conn.user.jid == conn.user.jid) {
+let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
+conn.reply(m.chat, stdout.toString(), m)}
+//} catch {
+var update = execSync('git remote set-url origin https://github.com/GataNina-Li/GataBot-MD.git && git pull')
+await m.reply(update.toString())
+} catch {
+await m.reply(`${fg}`) 
+}}
+handler.help = ['update']
+handler.tags = ['owner']
+handler.command = /^update|actualizar$/i
+handler.rowner = true
+export default handler 
