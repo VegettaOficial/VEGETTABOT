6d5821612d2ab2fb571e4690b5ff0ae8c4013ca2
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2023-12-01 11:45:32 -0500

    Update Prueba2.js

--- plugins/Prueba2.js
+++ plugins/Prueba2.js
@@ -1,13 +1,17 @@
-import {instagramdl} from "@bochilteam/scraper"
+import fetch from "node-fetch"
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0])
     throw `*_📌️ Uso del comando_*\n *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`;
   if (!args[0].match(/instagram/gi))
-    throw `❎ Asegurese que el enlace sea de Instagram`;
- 
-    let res = await instagramdl(args[0])
-    conn.sendFile(m.chat, res.medias.url, "igdl.mp4", `Resultado`, m)
-  }
+    throw `❎ Asegurese que el enlace sea de Instagram`
+ try {
+    const human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=GataDios&url=${args[0]}`)
+  const json = await human.json()
+  const videoig = json.result
+  await conn.sendFile(m.chat, videoig, 'error.mp4', `✅ ${mssg.lresult}`, m)
+  } catch {
+   m.reply('❎ Ocurrió un error en la descarga')
+ }
 }
 handler.help = ["instagram *<link ig>*"]
 handler.tags = ["downloader"]
