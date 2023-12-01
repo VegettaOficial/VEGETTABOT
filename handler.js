b1f0bf56b65651d82865f944603b79ebd54625b3
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2023-12-01 16:53:42 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -1456,10 +1456,10 @@ let chat = global.db.data.chats[msg?.chat] || {}
 if (!chat?.delete) return 
 if (!msg) return 
 if (!msg?.isGroup) return 
-const antideleteMessage = `*╭━━⬣ ${lenguajeGB['smsCont19']()} ⬣━━ 𓃠*
+const antideleteMessage = `╭━━⬣ ${lenguajeGB['smsCont19']()} ⬣━━
 ${lenguajeGB['smsCont20']()} @${participant.split`@`[0]}
 ${lenguajeGB['smsCont21']()}
-*╰━━━⬣ ${lenguajeGB['smsCont19']()} ⬣━━╯*`.trim();
+╰━━━⬣ ${lenguajeGB['smsCont19']()} ⬣━━╯`.trim();
 await mconn.conn.sendMessage(msg.chat, {text: antideleteMessage, mentions: [participant]}, {quoted: msg})
 mconn.conn.copyNForward(msg.chat, msg).catch(e => console.log(e, msg))
 } catch (e) {
