9cef293d159e82881462a635c62f30e3d68c1a81
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2023-12-02 18:03:32 -0500

    Update descargasig.js

--- plugins/Descargas/descargasig.js
+++ plugins/Descargas/descargasig.js
@@ -4,8 +4,8 @@ import fetch from 'node-fetch';
  import {instagram} from '@xct007/frieren-scraper'; 
  import {instagramdl} from '@bochilteam/scraper'; 
  const handler = async (m, {conn, args, command, usedPrefix}) => { 
-   if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`; 
-   m.reply(`*[❗] ᴅᴇsᴄᴀʀɢᴀɴᴅᴏ sᴜ ᴠɪᴅᴇᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ ᴘᴏʀ ғᴀᴠᴏʀ, ᴇsᴛᴇ ᴘʀᴏᴄᴇsᴏ ᴘᴜᴇᴅᴇ ᴅᴜʀᴀʀ ᴇɴᴛʀᴇ 2 ʏ 10 ᴍɪɴᴜᴛᴏs ᴅᴇᴘᴇɴᴅɪᴇɴᴅᴏ ᴅᴇ ʟᴀ ᴅᴜʀᴀᴄɪᴏɴ ᴅᴇʟ ᴠɪᴅᴇᴏ...*`); 
+   if (!args[0]) throw `⚠️ 𝙄𝙉𝙂𝙍𝙀𝙎𝘼 𝙀𝙇 𝙇𝙄𝙉𝙆 𝘿𝙀 𝙇𝘼 𝙄𝙈𝘼𝙂𝙀𝙉 𝙊 𝙑𝙄𝘿𝙀𝙊 𝘿𝙀 𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈.\n𝙀𝙟𝙚𝙢𝙥𝙡𝙤: ${usedPrefix + command} https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`; 
+   m.reply(`⭐ 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙉𝘿𝙊 𝙎𝙐 𝘼𝙍𝘾𝙃𝙄𝙑𝙊 𝘿𝙀 𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈, 𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍 𝙀𝙎𝙏𝙀 𝙋𝙍𝙊𝘾𝙀𝙎𝙊 𝙋𝙐𝙀𝘿𝙀 𝘿𝙐𝙍𝘼𝙍 𝙀𝙉𝙏𝙍𝙀 2 𝙈𝙄𝙉 𝘼 10 𝙈𝙄𝙉 𝘿𝙀𝙋𝙀𝙉𝘿𝙄𝙀𝙉𝘿𝙊 𝙀𝙇 𝘼𝙍𝘾𝙃𝙄𝙑𝙊 𝘼 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙍.`); 
    try { 
      const apiUrll = `https://api.betabotz.org/api/download/igdowloader?url=${encodeURIComponent(args[0])}&apikey=bot-secx3`; 
      const responsel = await axios.get(apiUrll); 
