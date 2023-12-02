4c1a5f114b7afc1fd12b2552cf88f177bc3d932c
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2023-12-01 20:33:10 -0500

    Update Prueba2.js

--- plugins/Prueba2.js
+++ plugins/Prueba2.js
@@ -1,20 +1,19 @@
-import fetch from "node-fetch"
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0])
-    throw `*_📌️ Uso del comando_*\n *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`;
-  if (!args[0].match(/instagram/gi))
-    throw `❎ Asegurese que el enlace sea de Instagram`
- try {
-    const human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=GataDios&url=${args[0]}`)
-  const json = await human.json()
-  const videoig = json.result
-  await conn.sendFile(m.chat, videoig, 'error.mp4', `✅ ${mssg.lresult}`, m)
+import {instagram} from '@xct007/frieren-scraper'
+let handler = async (m, {conn, args, usedPrefix, command}) => {
+  if (!args[0]) throw m.reply(`*_📌️ Uso del comando_*\n *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`)
+  if (!args[0].match(/instagram/gi)) throw m.reply(`❎ Asegurese que el enlace sea de Instagram`)
+try {
+  const datTa = await instagram.v1(args[0])
+  for (const urRRl of datTa) {
+  conn.sendFile(m.chat, urRRl.url, 'error.mp4', `✅Resultado`, m)
+  await new Promise((resolve) => setTimeout(resolve, 10000))
+    }
   } catch {
    m.reply('❎ Ocurrió un error en la descarga')
  }
 }
 handler.help = ["instagram *<link ig>*"]
 handler.tags = ["downloader"]
-handler.command = ["ig", "igdl", "instagram", "igimg", "igvid"]
+handler.command = ["instagram", "igdl", "igvid", "igimg", "ig"]
 
 export default handler
