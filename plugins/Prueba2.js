b8525f18960c05ec3035c44111cb5c1fc4f83b04
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2023-12-02 16:32:11 -0500

    Update Prueba2.js

--- plugins/Prueba2.js
+++ plugins/Prueba2.js
@@ -122,7 +122,7 @@ async function tts(text = 'error', lang = 'es') {
 return new Promise((resolve, reject) => {
 try {
 const tts = gtts(lang);
-const filePath = join(global.__dirname(import.meta.url), '../tmp', (1 * new Date) + '.wav');
+const filePath = join(global.__dirname(import.meta.url), '../../tmp', (1 * new Date) + '.wav');
 tts.save(filePath, text, () => {
 resolve(readFileSync(filePath));
 unlinkSync(filePath);
