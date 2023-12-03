eb26a5edb761f08d3f9adbf3e3ae0404d0033e16
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2023-12-02 20:52:43 -0500

    Update Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -1,35 +1,33 @@
-import Buscar from "lyria-npm";
-import axios from "axios";
-
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-  let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : "";
-  if (!teks) throw `⚠️️ *Ingrese el nombre de la canción.*`;
+import {find_lyrics} from '@brandond/findthelyrics';
+import {getTracks} from '@green-code/music-track-data';
+import {googleImage} from '@bochilteam/scraper';
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+  const teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : '';
+  if (!teks) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 𝙳𝙴 𝚄𝚂𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾: ${usedPrefix + command} beret ojala*`;
   try {
-    const letter = await Buscar(teks);
-    console.log(letter);
-    const result = `🍉 ↳ Titulo: ${letter.titulo}
-💿 ↳ Album: ${letter.albulm}
-
-${letter.letra}`;
-
-    /*await conn.sendUrl(m.chat, result, m, {
-        externalAdReply: {
-           mediaType: 1,
-           renderLargerThumbnail: true,
-           thumbnail: ftextjpg,
-           thumbnailUrl: ftextjpg,
-           title: botname,
-        }
-     })*/
-    m.reply(result);
+    const result = await getTracks(teks);
+    const lyrics = await find_lyrics(`${result[0].artist} ${result[0].title}`);
+    const res = await fetch(global.API('https://some-random-api.com', '/lyrics', {title: result[0].artist + result[0].title}));
+    const json = await res.json();
+    let img;
+    try {
+      img = result.album.artwork;
+    } catch {
+      try {
+        img = json.thumbnail.genius;
+      } catch {
+        const bochil = await googleImage(`${result[0].artist} ${result[0].title}`);
+        img = await bochil.getRandom();
+      }
+    }
+    const textoLetra = `🎤 𝚃𝙸𝚃𝚄𝙻𝙾: *${result[0].title || ''}*\n👤 𝙰𝚄𝚃𝙾𝚁: *${result[0].artist || ''}*\n\n📃🎵 𝙻𝙴𝚃𝚁𝙰:\n${lyrics || ''}`;
+    await conn.sendMessage(m.chat, {image: {url: img}, caption: textoLetra}, {quoted: m});
+    await conn.sendMessage(m.chat, {audio: {url: result[0].preview}, fileName: `${result[0].artist} ${result[0].title}.mp3`, mimetype: 'audio/mp4'}, {quoted: m});
   } catch {
-    throw `*_⚠️ Error, no se encontró la letra de esta canción._*`;
+    throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*`;
   }
 };
-
-handler.help = ["Lyrics"].map((v) => v + "");
-handler.tags = ["search"];
-handler.command = ["letraa", "lyrics", "letras"];
-handler.register = true;
-
+handler.help = ['lirik', 'letra'].map((v) => v + ' <Apa>');
+handler.tags = ['internet'];
+handler.command = /^(lirik|lyrics|lyric|letraa)$/i;
 export default handler;
