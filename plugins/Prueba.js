1f80b089f7cb3a52a3c23bb960f5b1c8dd9699c0
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2023-12-02 20:47:52 -0500

    Create Prueba.js

---
+++ plugins/Prueba.js
@@ -0,0 +1,35 @@
+import Buscar from "lyria-npm";
+import axios from "axios";
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : "";
+  if (!teks) throw `⚠️️ *Ingrese el nombre de la canción.*`;
+  try {
+    const letter = await Buscar(teks);
+    console.log(letter);
+    const result = `🍉 ↳ Titulo: ${letter.titulo}
+💿 ↳ Album: ${letter.albulm}
+
+${letter.letra}`;
+
+    /*await conn.sendUrl(m.chat, result, m, {
+        externalAdReply: {
+           mediaType: 1,
+           renderLargerThumbnail: true,
+           thumbnail: ftextjpg,
+           thumbnailUrl: ftextjpg,
+           title: botname,
+        }
+     })*/
+    m.reply(result);
+  } catch {
+    throw `*_⚠️ Error, no se encontró la letra de esta canción._*`;
+  }
+};
+
+handler.help = ["Lyrics"].map((v) => v + "");
+handler.tags = ["search"];
+handler.command = ["letra", "lyrics", "letras"];
+handler.register = false;
+
+export default handler;
