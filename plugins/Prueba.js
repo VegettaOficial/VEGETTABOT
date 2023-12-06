de86fed41e6c97eae751885fd1bac6b29e49f197
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2023-12-05 22:08:03 -0500

    Update Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -1,225 +1,139 @@
-import fetch from "node-fetch";
-import axios from "axios";
-import { translate } from "@vitalets/google-translate-api";
-let handler = async (m, { conn, command }) => {
-  if (command == "consejo") {
-    let res = await fetch("https://api.akuari.my.id/randomtext/bacot");
-    let json = await res.json();
-    let frase = await translate(`${json.hasil.result}`, {
-      to: "es",
-      autoCorrect: true,
-    });
-
-    m.reply(`*┏━━━━━━━━━━━━━━━━┓*\n*┠❧  ${frase}*\n*┗━━━━━━━━━━━━━━━━┚*`);
+import translate from '@vitalets/google-translate-api';
+import fetch from 'node-fetch';
+const handler = async (m, {conn, command}) => {
+  if (command === 'consejo') {
+    const consejo = consejos[Math.floor(Math.random() * consejos.length)];
+    const mensaje = `╭─◆────◈⚘◈─────◆─╮\n\n⠀⠀🌟 *Consejo del día* 🌟\n\n❥ ${consejo}\n\n╰─◆────◈⚘◈─────◆─╯`;
+    await m.reply(mensaje);
   }
 
-  if (command == "fraseromantica") {
-    let res = await fetch("https://api.popcat.xyz/pickuplines");
-    let json = await res.json();
-    let { pickupline } = json;
-    const tld = "cn";
-    let frase = await translate(`${pickupline}`, {
-      to: "es",
-      autoCorrect: true,
-    });
-    let fraseromantica = `*❥  ${frase.text}*`;
-    conn.sendNyanCat(
-      m.chat,
-      fraseromantica,
-      await (
-        await fetch("https://telegra.ph/file/62ef85167cc5015d4b40c.jpg")
-      ).buffer(),
-      "Fʀᴀsᴇs Bʏ NʏᴀɴCᴀᴛBᴏᴛ 🌺",
-      "◍⃝߭۫ꯨ🐢۪〬.࠭⤿ Is Magic🐱",
-      script,
-      m
-    );
+  if (command === 'fraseromantica') {
+    const frase_romantica = frasesromanticas[Math.floor(Math.random() * frasesromanticas.length)];
+    const mensaje = `╭─◆────◈⚘◈─────◆─╮\n\n⠀⠀💖 *Frase romántica* 💖\n\n❥ ${frase_romantica}\n\n╰─◆────◈⚘◈─────◆─╯`;
+    await m.reply(mensaje);
   }
 
-  if (command == "frase") {
-    let res = await fetch(
-      "https://api.zahwazein.xyz/randomtext/randomquote?apikey=zenzkey_048b3b850d51"
-    );
-    let json = await res.json();
-    let { message } = json.result;
-    const tld = "cn";
-    let frase = await translate(`${message}`, { to: "es", autoCorrect: true });
-    conn.sendNyanCat(
-      m.chat,
-      frase.text,
-      await (
-        await fetch("https://telegra.ph/file/f6a339acb8b55b33c99dc.jpg")
-      ).buffer(),
-      "Fʀᴀsᴇs Bʏ NʏᴀɴCᴀᴛBᴏᴛ 🌺",
-      "◍⃝߭۫ꯨ🐢۪〬.࠭⤿ Is Magic🐱",
-      script,
-      m
-    );
-  }
-  if (command == "dato") {
-    let res = await fetch("https://api.popcat.xyz/fact");
-    let json = await res.json();
-    let { fact } = json;
-    let frase = await translate(`${fact}`, { to: "es", autoCorrect: true });
-    conn.sendNyanCat(
-      m.chat,
-      frase.text,
-      await (
-        await fetch("https://telegra.ph/file/bc98459abf3e9b050cb1b.jpg")
-      ).buffer(),
-      "Fʀᴀsᴇs Bʏ NʏᴀɴCᴀᴛBᴏᴛ 🌺",
-      "◍⃝߭۫ꯨ🐢۪〬.࠭⤿ Is Magic🐱",
-      script,
-      m
-    );
-  }
-  if (command == "frasesabia") {
-    let res = await fetch("https://api.akuari.my.id/randomtext/katabijak");
-    let json = await res.json();
-    let { quotes } = json.hasil;
-    let frase = await translate(`${quotes}`, { to: "es", autoCorrect: true });
-    conn.sendNyanCat(
-      m.chat,
-      frase.text,
-      await (
-        await fetch("https://telegra.ph/file/31a2a876b8cd95cfd4580.jpg")
-      ).buffer(),
-      "Fʀᴀsᴇs Bʏ NʏᴀɴCᴀᴛBᴏᴛ 🌺",
-      "◍⃝߭۫ꯨ🐢۪〬.࠭⤿ Is Magic🐱",
-      script,
-      m
-    );
-  }
-  if (command == "pensamiento") {
-    let res = await fetch("https://api.akuari.my.id/randomtext/katailham");
-    let json = await res.json();
-    let { result } = json.hasil;
-    let frase = await translate(`${result}`, { to: "es", autoCorrect: true });
-    conn.sendNyanCat(
-      m.chat,
-      frase.text,
-      await (
-        await fetch("https://telegra.ph/file/20f8e173cde318c845d92.jpg")
-      ).buffer(),
-      "Fʀᴀsᴇs Bʏ NʏᴀɴCᴀᴛBᴏᴛ 🌺",
-      "◍⃝߭۫ꯨ🐢۪〬.࠭⤿ Is Magic🐱",
-      script,
-      m
-    );
-  }
-  if (command == "critico") {
-    let res = await fetch("https://api.akuari.my.id/randomtext/sindiran");
-    let json = await res.json();
-    let { result } = json.hasil;
-    let frase = await translate(`${result}`, { to: "es", autoCorrect: true });
-    conn.sendNyanCat(
-      m.chat,
-      frase.text,
-      await (
-        await fetch("https://telegra.ph/file/b1644222c0b6ea691cad7.jpg")
-      ).buffer(),
-      "Fʀᴀsᴇs Bʏ NʏᴀɴCᴀᴛBᴏᴛ 🌺",
-      "◍⃝߭۫ꯨ🐢۪〬.࠭⤿ Is Magic🐱",
-      script,
-      m
-    );
-  }
-  if (command == "chiste") {
-    let res = await fetch("https://api.popcat.xyz/joke");
-    let json = await res.json();
-    let { joke } = json;
-    let frase = await translate(`${joke}`, { to: "es", autoCorrect: true });
-    conn.sendNyanCat(
-      m.chat,
-      frase.text,
-      await (
-        await fetch("https://telegra.ph/file/0b419d5ac4c2c3ec92344.jpg")
-      ).buffer(),
-      "Fʀᴀsᴇs Bʏ NʏᴀɴCᴀᴛBᴏᴛ 🌺",
-      "◍⃝߭۫ꯨ🐢۪〬.࠭⤿ Is Magic🐱",
-      script,
-      m
-    );
-  }
-  if (command == "2opciones") {
-    let res = await fetch("https://api.popcat.xyz/wyr");
-    let json = await res.json();
-    let { ops1, ops2 } = json;
-    let frase = await translate(`${ops1}`, { to: "es", autoCorrect: true });
-    let frase2 = await translate(`${ops2}`, { to: "es", autoCorrect: true });
-    conn.sendNyanCat(
-      m.chat,
-      `*Opción 1:* ${frase.text}\n\n*O*\n\n*Opción 2:* ${frase2.text}`,
-      await (
-        await fetch("https://telegra.ph/file/80cd78c02095417328c42.jpg")
-      ).buffer(),
-      "Fʀᴀsᴇs Bʏ NʏᴀɴCᴀᴛBᴏᴛ 🌺",
-      "◍⃝߭۫ꯨ🐢۪〬.࠭⤿ Is Magic🐱",
-      script,
-      m
-    );
-  }
-    if (command == "adivinanza") {
-    let res = await fetch("https://riddles-api.vercel.app/random");
-    let json = await res.json();
-    let { riddle, answer } = json;
-    let frase = await translate(`${riddle}`, { to: "es", autoCorrect: true });
-    let frase2 = await translate(`${answer}`, { to: "es", autoCorrect: true });
-    conn.sendNyanCat(
-      m.chat,
-      `*${frase.text}*\n\n${frase2.text}`,
-      await (
-        await fetch("https://telegra.ph/file/d6f67ab577ebb73b9f989.jpg")
-      ).buffer(),
-      "Fʀᴀsᴇs Bʏ NʏᴀɴCᴀᴛBᴏᴛ 🌺",
-      "◍⃝߭۫ꯨ🐢۪〬.࠭⤿ Is Magic🐱",
-      script,
-      m
-    );
-  }
-      if (command == "famososfrase") {
-    const options = {
-  method: 'GET',
-  url: 'https://famous-quotes4.p.rapidapi.com/random',
-  params: {
-    category: 'all',
-    count: '2'
-  },
-  headers: {
-    'X-RapidAPI-Key': '44cbeee30emsha89b91b41a56cfcp15fb63jsn7601bd3a62fa',
-    'X-RapidAPI-Host': 'famous-quotes4.p.rapidapi.com'
+  if (command == 'romantica') {
+    try {
+      const cerpe = await cerpen(`cinta romantis`);
+      const storytime = await translate(cerpe.cerita, {to: 'es', autoCorrect: true}).catch((_) => null);
+      const titletime = await translate(cerpe.title, {to: 'es', autoCorrect: true}).catch((_) => null);
+      conn.reply(m.chat, `᭥🫐᭢ Título: ${titletime.text}
+᭥🍃᭢ Autor: ${cerpe.author}
+────────────────
+${storytime.text}`, m);
+    } catch {
+      const err = await fetch(`https://api.popcat.xyz/pickuplines`);
+      const json = await err.json();
+      const fraseChat = json.result;
+      conn.reply(m.chat, fraseChat, m);
+    }
   }
 };
-	const response = await axios.request(options);
-    let frase = await translate(`${response.data[0].text}`, { to: "es", autoCorrect: true });
-    let frase2 = await translate(`${response.data[0].category}`, { to: "es", autoCorrect: true });
-    conn.sendNyanCat(
-      m.chat,
-      `🪴 Frase:  ${frase.text}\n*🌺 Autor:* ${response.data[0].author}\n*☕ Categoría:* ${frase2.text}`,
-      await (
-        await fetch("https://telegra.ph/file/1e8ceea60b76d1a77dcd0.jpg")
-      ).buffer(),
-      "Fʀᴀsᴇs Bʏ NʏᴀɴCᴀᴛBᴏᴛ 🌺",
-      "◍⃝߭۫ꯨ🐢۪〬.࠭⤿ Is Magic🐱",
-      script,
-      m
-    );
-  }
-};
-handler.tags = ["frases"];
-handler.command = handler.help = [
-  "consejo",
-  "fraseromantica",
-  "frase",
-  "dato",
-  "frasesabia",
-  "pensamiento",
-  "critico",
-  "chiste",
-  "2opciones",
-  "adivinanza",
-  "famososfrase",
-];
-handler.register = true;
-
+handler.tags = ['frases'];
+handler.command = handler.help = ['consejo', 'fromantica', 'romantica'];
 export default handler;
+
+async function cerpen(category) {
+  return new Promise((resolve, reject) => {
+    const title = category.toLowerCase().replace(/[()*]/g, '');
+    const judul = title.replace(/\s/g, '-');
+    const page = Math.floor(Math.random() * 5);
+    axios.get('http://cerpenmu.com/category/cerpen-'+judul+'/page/'+page)
+        .then((get) => {
+          const $ = cheerio.load(get.data);
+          const link = [];
+          $('article.post').each(function(a, b) {
+            link.push($(b).find('a').attr('href'));
+          });
+          const random = link[Math.floor(Math.random() * link.length)];
+          axios.get(random).then((res) => {
+            const $$ = cheerio.load(res.data);
+            const hasil = {
+              title: $$('#content > article > h1').text(),
+              author: $$('#content > article').text().split('Cerpen Karangan: ')[1].split('Kategori: ')[0],
+              kategori: $$('#content > article').text().split('Kategori: ')[1].split('\n')[0],
+              lolos: $$('#content > article').text().split('Lolos moderasi pada: ')[1].split('\n')[0],
+              cerita: $$('#content > article > p').text(),
+            };
+            resolve(hasil);
+          });
+        });
+  });
+}
+
+global.romanticas = [
+  'Eres la luz que ilumina mi vida en la oscuridad.',
+  'Contigo, cada día es una nueva aventura llena de amor.',
+  'Tus ojos son el reflejo del cielo en el que quiero perderme.',
+  'Cada latido de mi corazón lleva tu nombre.',
+  'En tus brazos encontré el hogar que siempre busqué.',
+  'Eres el sueño que nunca quiero despertar.',
+  'El amor verdadero es estar juntos en las buenas y en las malas.',
+  'No existen distancias cuando dos corazones están unidos.',
+  'Tus besos son la melodía que acelera mi corazón.',
+  'Amar es ver en ti lo que nadie más puede ver.',
+  'En cada latido, te llevo conmigo a todas partes.',
+  'El amor que siento por ti es mi fuerza y mi inspiración.',
+  'Tus palabras dulces son mi alimento emocional diario.',
+  'Eres el regalo más preciado que la vida me ha dado.',
+  'El tiempo se detiene cuando estoy junto a ti.',
+  'En tu sonrisa encuentro la felicidad que buscaba.',
+  'Cada día a tu lado es una historia de amor sin fin.',
+  'Nuestro amor es como un cuento de hadas hecho realidad.',
+  'Tus abrazos son mi refugio en este mundo caótico.',
+  'Eres la razón por la que creo en el destino.',
+  'Amar es descubrir cada día algo nuevo que admiro en ti.',
+  'Tu amor es el lienzo en blanco donde pinto mi felicidad.',
+  'Contigo, el futuro es un camino lleno de promesas y sueños.',
+  'Eres el faro que guía mi corazón en la oscuridad.',
+  'La magia del amor se encuentra en cada gesto que compartimos.',
+  'Nuestro amor es un baile eterno de pasión y ternura.',
+  'En tus brazos, el mundo entero desaparece y solo existimos tú y yo.',
+  'El amor es el idioma en el que nuestros corazones conversan.',
+  'Eres el pedacito que me faltaba para completar mi alma.',
+  'Amar es encontrar en ti todo lo que nunca supe que necesitaba.',
+];
+
+global.consejos = [
+  'Acepta que los cambios son parte natural de la vida, y aprende a adaptarte a ellos.',
+  'Nunca dejes de aprender; el conocimiento es una herramienta poderosa.',
+  'Cuida de tu salud física y mental, son fundamentales para una vida plena.',
+  'Disfruta de las pequeñas cosas, pues son ellas las que dan sentido a la vida.',
+  'Aprende a perdonar, tanto a los demás como a ti mismo, para liberar tu corazón.',
+  'Valora el tiempo que pasas con tus seres queridos, es el regalo más valioso que puedes dar y recibir.',
+  'Sé amable y compasivo con los demás, cada acto de bondad puede marcar la diferencia en sus vidas.',
+  'Aprende a decir \'no\' cuando sea necesario, y establece límites saludables.',
+  'Encuentra tiempo para hacer lo que te apasiona, pues eso nutre tu alma y te hace sentir vivo.',
+  'No te compares con los demás, cada persona tiene su propio camino y ritmo en la vida.',
+  'Escucha a tu pareja con empatía y comprensión, la comunicación es la base de una relación sólida.',
+  'No tengas miedo de expresar tus sentimientos, la honestidad es esencial en el amor.',
+  'Aprende a ceder y a comprometerte, el amor requiere de sacrificio y esfuerzo mutuo.',
+  'Sorprende a tu pareja de vez en cuando, mantén viva la chispa del romance.',
+  'Respeta la individualidad de tu pareja y permítele crecer como persona.',
+  'El amor propio es igual de importante que amar a alguien más; cuídate y valórate.',
+  'Recuerda que una relación sana se basa en la confianza mutua y el respeto.',
+  'Elige a alguien que te complemente y te haga ser una mejor versión de ti mismo.',
+  'El amor verdadero no te hace sentir menos, te hace sentir más.',
+  'Amar es un verbo, es una elección diaria que se cultiva con acciones y palabras.',
+  'Encuentra un trabajo que te apasione, y nunca sentirás que estás trabajando.',
+  'Sé proactivo y toma la iniciativa en tu trabajo, eso será valorado por tus superiores.',
+  'Aprende de tus errores y fracasos, son oportunidades para crecer y mejorar.',
+  'Mantén una actitud positiva y busca soluciones ante los desafíos laborales.',
+  'Cultiva buenas relaciones con tus colegas, el trabajo en equipo es clave para el éxito.',
+  'Establece metas claras y realistas, y trabaja con determinación para alcanzarlas.',
+  'No tengas miedo de pedir ayuda o buscar mentoría, siempre hay algo nuevo que aprender.',
+  'Reconoce y valora tus logros, celebra tus éxitos por pequeños que sean.',
+  'Busca un equilibrio entre tu vida laboral y personal, ambos aspectos son importantes.',
+  'El trabajo es una parte importante de tu vida, pero no es lo único que define quién eres.',
+  'Cree en ti mismo y en tu capacidad para lograr lo que te propongas.',
+  'Visualiza tus metas y sueños, imagina cómo te sentirás al alcanzarlos.',
+  'Encuentra inspiración en aquellos que han superado obstáculos similares a los tuyos.',
+  'Acepta los fracasos como parte del proceso, son oportunidades para aprender y crecer.',
+  'Rodéate de personas positivas y que te impulsen hacia adelante.',
+  'Mantén una mentalidad abierta y dispuesta a aprender cosas nuevas.',
+  'Recuerda por qué empezaste cuando te sientas desmotivado; reconecta con tu propósito.',
+  'Divide tus metas en pequeños pasos, eso hará el camino más alcanzable y menos abrumador.',
+  'No tengas miedo de perseguir tus sueños, la vida es demasiado corta para vivir con arrepentimientos.',
+  'Confía en que, con esfuerzo y perseverancia, puedes lograr todo lo que te propongas.',
+];
