7d453062e0facdb9faef65d9ccf5ba6635e9a5b1
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2023-12-06 00:46:30 -0500

    Update Agendasemanal.js

--- plugins/Free fire/Agendasemanal.js
+++ plugins/Free fire/Agendasemanal.js
@@ -2,14 +2,14 @@ let handler = async (m, { isPrems, conn }) => {
 let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
 if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
 
-let img = 'https://telegra.ph/file/f296b3635a5ae306f9bf7.jpg' 
+let img = 'https://telegra.ph/file/1d36c0161f5ffff7dcded.jpg' 
 let texto = `📕 ¡LA #AGENDASEMANAL ESTÁ AQUÍ! 📕
 
-🛑 Alerta: 🌜 ¡LUNA SANGRIENTA ESTÁ DE VUELTA! Aparta la fecha y ve por los 4 aspectos de la colección en la Ruleta de Tokens.
+🚗💨 Evoluciona esta semana: ¡Los 7 niveles de la EVO AN94 está AQUÍ!
 
-🪐 Además, emprende una Odisea Galáctica con la NUEVA TEMPORADA del Pase BOOYAH! y revoluciona tu arsenal con la velocidad de la M1887 y la katana Poder Esmeralda. 🟩
+💥🏎 Aduéñate del camino con #FreefireLamborghini y llévate todos los vehículos, pared gloo y el emote que no se detiene en movimiento. 🔥Además, enciende el Fuego Glacial con la granada y el ala delta especial.
 
-💥 ¡Entra ahora! Poder y recompensas te esperan.
+❓ ¿Qué objeto llevarás a la batalla?
 `
 
 const fkontak = {
