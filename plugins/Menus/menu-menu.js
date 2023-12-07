e454574c2ebcdd499a263b7290df07136f883f7d
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2023-12-07 13:52:33 -0500

    Update menu-menu.js

--- plugins/Menus/menu-menu.js
+++ plugins/Menus/menu-menu.js
@@ -465,7 +465,7 @@ let menu = `
 ╰━━━━━━⋆★⋆━━━━━━⬣
  `.trim()
     
-const vi = ['https://telegra.ph/file/640b01e3b0469794b3f28.mp4']
+const vi = ['https://telegra.ph/file/be01122a7eea4915c7317.mp4']
 
 try {
 await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak }) 
