e721598ed84072a4d3fb761322f3eb6848a301eb
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2023-12-09 15:54:05 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -947,7 +947,7 @@ global.db.data.chats[m.chat] = {}
 if (chat) {
 if (!('isBanned' in chat)) chat.isBanned = false                    
 if (!('welcome' in chat)) chat.welcome = true                    
-if (!('detect' in chat)) chat.detect = true                    
+if (!('detect' in chat)) chat.detect = false                    
 if (!('sWelcome' in chat)) chat.sWelcome = ''                    
 if (!('sBye' in chat)) chat.sBye = ''                    
 if (!('sPromote' in chat)) chat.sPromote = ''                    
@@ -981,7 +981,7 @@ if (!isNumber(chat.expired)) chat.expired = 0
 global.db.data.chats[m.chat] = {
 isBanned: false,
 welcome: true,
-detect: true,
+detect: false,
 sWelcome: '',
 sBye: '',
 sPromote: '',
