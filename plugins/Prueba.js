cc082510853cd9d01bc6a3de98d997850c2c771a
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2023-12-11 21:56:22 -0500

    Update Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -1,7 +1,7 @@
 
 let handler = async (m, { conn, args, groupMetadata}) => {
        let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-       if (!(who in global.db.data.users)) throw `✳️ ${mssg.userDb}`
+       if (!(who in global.db.data.users)) throw `✳️ hh`
        let warn = global.db.data.users[who].warn
        let name = conn.getName(who)
       m.reply(`
