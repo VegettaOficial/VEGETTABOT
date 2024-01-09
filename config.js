efb03d4f084d6326d248834b60bef0a1bf56bc1d
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2024-01-09 09:16:33 -0500

    Update config.js

--- config.js
+++ config.js
@@ -61,7 +61,7 @@ global.itsrose = ["4b146102c4d500809da9d1ff"];
 global.baileys = "@whiskeysockets/baileys";
 
 global.APIs = { 
-  ApiEmpire: 'https://api-ash.zipponodes.xyz',
+  CFROSAPI: 'https://api.cafirexos.com',
   xteam: 'https://api.xteam.xyz',
   dzx: 'https://api.dhamzxploit.my.id',
   lol: 'https://api.lolhuman.xyz',
