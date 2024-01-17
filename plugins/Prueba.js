797ff3470a9babeda797c3db9f761dd43fa992b0
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2024-01-17 17:35:49 -0500

    Update Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -1,6 +1,6 @@
 var handler = async (m, { conn, participants, groupMetadata, args, text }) => {
 
-const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => './src/novios.jpg')
+  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/novios.jpg')
 const groupAdmins = participants.filter(p => p.admin)
 const listaAdmins = groupAdmins.map((v, i) => ``).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
