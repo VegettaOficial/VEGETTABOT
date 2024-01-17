3f0c7a07841f122649acf49d2df6c76f7449b121
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2024-01-17 16:20:02 -0500

    Update Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -2,18 +2,27 @@ var handler = async (m, { conn, participants, groupMetadata, args, text }) => {
 
 const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './storage/menus/Menu2.jpg'
 const groupAdmins = participants.filter(p => p.admin)
-const listaAdmins = groupAdmins.map((v, i) => `4VS4 - - - -`).join('\n')
+const listaAdmins = groupAdmins.map((v, i) => ``).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 if (!text) return m.reply(`Ingrese la hora`)
 if (text.length < 0) return m.reply(`*⚠️ EL MOTIVO ES MUY CORTO, MINIMO 10 CARÁCTERES*`)
 let mensaje = args.join` `
 let yo = `Hora: *${text}*`
-let texto = `[❗] 𝙈𝙀𝙉𝙎𝘼𝙅𝙀 𝙋𝘼𝙍𝘼 𝘼𝘿𝙈𝙄𝙉𝙎 
+let texto = `4vs4 COMPE 
+Reglas : CLK
 
-${yo}
+𝐂𝐎𝐍𝐅𝐈𝐑𝐌𝐀𝐑 𝐀𝐒𝐈𝐒𝐓𝐄𝐍𝐂𝐈𝐀:
+🥷🏻|
+🥷🏻|  
+🥷🏻|  
+🥷🏻| 
 
-𝘼𝘿𝙈𝙄𝙉𝙄𝙎𝙏𝙍𝘼𝘿𝙊𝙍𝙀𝙎 :
-${listaAdmins}
+𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄𝐒:
+🥷🏻|
+🥷🏻|
+
+𝐃𝐎𝐍𝐀𝐃𝐎𝐑 𝐃𝐄 𝐒𝐀𝐋𝐀:
+👨🏻‍💼|
 
 *╰━* 𝙀𝙇𝙄𝙏𝙀 𝘽𝙊𝙏 𝙂𝙇𝙊𝘽𝘼𝙇
 ▌│█║▌║▌║║▌║▌║▌║█`.trim()
@@ -22,7 +31,7 @@ conn.sendFile(m.chat, pp, 'error.jpg', texto, m, false, { mentions: [...groupAdm
 }
 handler.help = ['admins']
 handler.tags = ['grupo']
-handler.command = /^(2x2)$/i
+handler.command = /^(2x2clk)$/i
 handler.group = true
 
 export default handler
