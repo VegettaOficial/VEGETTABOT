727e82d4ff117675c82c599c857819a619011a77
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2024-01-17 19:26:03 -0500

    Update Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -2,22 +2,35 @@ var handler = async (m, { conn, participants, groupMetadata, args, text }) => {
 
 const pp = './src/4vs4clk.png'
 const groupAdmins = participants.filter(p => p.admin)
-const listaAdmins = groupAdmins.map((v, i) => `┃👤 @${v.id.split('@')[0]}`).join('\n')
+const listaAdmins = groupAdmins.map((v, i) => ``).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 if (!text) return m.reply(`⚠️ 𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙃𝙊𝙍𝘼𝙍𝙄𝙊.\n𝙀𝙟𝙚𝙢𝙥𝙡𝙤:\n.4𝘷𝘴4𝘤𝘭𝘬 5𝘱𝘮 𝘌𝘤 / 4𝘱𝘮 𝘔𝘹`)
 if (text.length < 0) return m.reply(`*⚠️ EL MOTIVO ES MUY CORTO, MINIMO 10 CARÁCTERES*`)
 let mensaje = args.join` `
-let yo = `🕓 𝙃𝙊𝙍𝘼: *${text}*`
+let yo = `🕓 𝐇𝐎𝐑𝐀: *${text}*`
 let texto = `╭──────⚔──────╮
    4𝐕𝐒4 𝐂𝐎𝐌𝐏𝐄𝐓𝐈𝐓𝐈𝐕𝐎 
 ╰──────⚔──────╯
 
 ${yo}
+📑 𝐑𝐄𝐆𝐋𝐀𝐒: 𝐂𝐋𝐊
 
-𝘼𝘿𝙈𝙄𝙉𝙄𝙎𝙏𝙍𝘼𝘿𝙊𝙍𝙀𝙎 :
-${listaAdmins}
-
-`.trim()
+╭──────────────╮
+│ 𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 𝟏
+│👑 ➤ 
+│🥷🏻 ➤  
+│🥷🏻 ➤ 
+│🥷🏻 ➤ 
+╰─────────────╯
+╭──────────────╮
+│ 𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 𝟐
+│👑 ➤ 
+│🥷🏻 ➤  
+│🥷🏻 ➤ 
+│🥷🏻 ➤ 
+╰─────────────╯
+𝙀𝙇𝙄𝙏𝙀 𝘽𝙊𝙏 𝙂𝙇𝙊𝘽𝘼𝙇 
+❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘`.trim()
 conn.sendFile(m.chat, pp, 'error.jpg', texto, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })
 
 }
