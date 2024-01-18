cf5baf4dabacbbc4a65fc3ffe8a54009c484ca95
Jeffo <129682184+ELITEBOT15@users.noreply.github.com>
2024-01-18 04:49:51 -0500

    Update Prueba6.js

--- plugins/Prueba6.js
+++ plugins/Prueba6.js
@@ -33,7 +33,7 @@ console.log(e)
 }}
 handler.help = ['addprem <@user>']
 handler.tags = ['owner']
-handler.command = /^(advertir|advertencia|Advertencias|warn|warning)$/i
+handler.command = /^(lark)$/i
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
