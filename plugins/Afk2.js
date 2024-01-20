ca3b5330341a6b7c490266de6b4d5641d20324e7
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-01-19 23:05:15 -0500

    Update Afk2.js

--- plugins/Afk2.js
+++ plugins/Afk2.js
@@ -1,20 +1,19 @@
-var handler = async (m, { text }) => {
-  const user = global.db.data.users[m.sender];
-  user.afk = +new Date();
-  user.afkReason = text;
-  const afkinfo = `
-    *DORRAT - AFK*
-    
-    *👤 USUARIO QUE ESTARA AFK: ${conn.getName(m.sender)}*
-    *❍ MOTIVO (AFK): ${text ? ": " + text : ""}*
-    *❍ NO LO ETIQUETEN*
-  `;
-  await m.reply(afkinfo);
-};
+let handler = async (m, { text, args, usedPrefix, command, conn}) => { 
+let user = global.db.data.users[m.sender]
+  
+if (args.length >= 1) {
+text = args.slice(0).join(" ")
+} else if (m.quoted && m.quoted.text) {
+text = m.quoted.text
+} else return m.reply(`${lenguajeGB['smsAfkQ1'](usedPrefix, command)}`)
+  
+if (text.length < 10) return m.reply(`${lenguajeGB['smsAfkQ2']()}`)
+user.afk = + new Date
+user.afkReason = text
+await conn.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}✴️ *A F K* ✴️
+*▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔*
+${lenguajeGB['smsAfkM1A']()} *@${m.sender.split("@")[0]}* ${lenguajeGB['smsAfkM1B']()}${text ? '\n👉 ' + text : ''}`, m, { mentions: [m.sender] })
+}
 
-handler.help = ["afk [alasan]"];
-handler.tags = ["main"];
-handler.command = /^afk$/i;
-handler.dolares = 10;
-
+handler.command = /^afk$/i
 export default handler
