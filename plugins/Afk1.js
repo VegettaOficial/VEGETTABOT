103a6ec523ebb8988bb4db44e74b4bfaa7c4d6ce
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-01-19 22:51:43 -0500

    Update Afk1.js

--- plugins/Afk1.js
+++ plugins/Afk1.js
@@ -1,28 +1,30 @@
 export function before(m) {
   const user = global.db.data.users[m.sender];
-  if (user.afk > -1) {
+
+  if (user && user.afk > -1) {
     m.reply(
       `
-    *🔷 DEJASTE DE ESTAR AFK ${user.afkReason ? ` MOTIVO:\n ${user.afkReason}` : ""}*
-    
+    *🔷 DEJASTE DE ESTAR AFK${user.afkReason ? `\nMOTIVO: ${user.afkReason}` : ""}*
     *⏳ TIEMPO DE INACTIVIDAD AFK: ${(new Date() - user.afk).toTimeString()}*
   `.trim()
     );
+
     user.afk = -1;
     user.afkReason = "";
   }
 
   const jids = [...new Set([...m.mentionedJid, ...(m.quoted ? [m.quoted.sender] : [])])].filter(jid => global.db.data.users[jid]);
+
   for (const jid of jids) {
-    const afkTime = user.afk;
-    if (!afkTime || afkTime < 0) continue;
-    const reason = user.afkReason || "";
-    m.reply(
-      `*❍ NO LO ETIQUETES, ESTA (INACTIVO) (𝙰𝙵𝙺)*      
-*❍ ${reason ? `MOTIVO (AFK): ${reason}` : "MOTIVO (AFK): _EL USUARIO NO ESPECIFICA UN MOTIVO_"}*
-*❍ TIEMPO INACTIVO (AFK): ${(new Date() - afkTime).toTimeString()}*
-  `.trim()
-    );
+    const afkTime = global.db.data.users[jid]?.afk;
+
+    if (afkTime && afkTime > -1) {
+      const reason = global.db.data.users[jid].afkReason || "";
+      m.reply(
+        `*❍ NO LO ETIQUETES, ESTÁ (INACTIVO) (𝙰𝙵𝙺)*\n*❍ ${reason ? `MOTIVO (AFK): ${reason}` : "MOTIVO (AFK): _EL USUARIO NO ESPECIFICA UN MOTIVO_"}*\n*❍ TIEMPO INACTIVO (AFK): ${(new Date() - afkTime).toTimeString()}*`.trim()
+      );
+    }
   }
+
   return true;
 }
