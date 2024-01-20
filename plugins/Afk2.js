2ba26ef159b51b5ee93c40bacc20d8adc2a188ba
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-01-19 23:39:31 -0500

    Update Afk2.js

--- plugins/Afk2.js
+++ plugins/Afk2.js
@@ -5,7 +5,7 @@ if (args.length >= 1) {
 text = args.slice(0).join(" ")
 } else if (m.quoted && m.quoted.text) {
 text = m.quoted.text
-} else return m.reply(`${lenguajeGB['smsAfkQ1'](usedPrefix, command)}`)
+} else return m.reply(`${lenguajeGB['smsAfkQ2'](usedPrefix, command)}`)
   
 if (text.length < 10) return m.reply(`${lenguajeGB['smsAfkQ2']()}`)
 user.afk = + new Date
