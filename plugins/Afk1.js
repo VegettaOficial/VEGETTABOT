c640454e30bf1e9009144725e34d20267907d77e
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-01-22 16:44:08 -0500

    Update Afk1.js

--- plugins/Afk1.js
+++ plugins/Afk1.js
@@ -1,30 +1,12 @@
-let handler = m => m 
-handler.before = async function (m, { text, args, usedPrefix, command, conn } ) {
-let user = global.db.data.users[m.sender]
-if (user.afk > -1) {
-await conn.reply(m.chat, `${lenguajeGB['smsAvisoEG']()}✴️ *A F K* ✴️
-*▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔*
-*@${m.sender.split("@")[0]}* ${lenguajeGB['smsAfkM1']()}${user.afkReason ? `\n${lenguajeGB['smsAfkM2']()}👉 ` + user.afkReason : ''}
-
-${lenguajeGB['smsAfkM3']()}\n👉 *${(new Date - user.afk).toTimeString()}*`.trim(), m, { mentions: [m.sender] })
-user.afk = -1
-user.afkReason = ''
-}
-let jids = [...new Set([...(m.mentionedJid || []), ...(m.quoted ? [m.quoted.sender] : [])])]
-for (let jid of jids) {
-let user = global.db.data.users[jid]
-if (!user)
-continue
-let afkTime = user.afk
-if (!afkTime || afkTime < 0)
-continue
-let reason = user.afkReason || ''
-await conn.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}✴️ *A F K* ✴️
-*▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔*
-😾 ${lenguajeGB['smsAfkM4']()}\n${reason ? `${lenguajeGB['smsAfkM5']()}` + '👉 ' + reason : `${lenguajeGB['smsAfkM6']()}`}
-
-${lenguajeGB['smsAfkM3']()}\n👉 *${(new Date - user.afk).toTimeString()}*`.trim(), m)
-}
-return true
-}
-export default handler
+import uploadImage from '../lib/uploadImage.js';
+const handler = async (m, {conn, text, usedPrefix, command, isOwner}) => {
+  const q = m.quoted ? m.quoted : m;
+  const mime = (q.msg || q).mimetype || '';
+  if (!mime) throw '*A que imagen hago archivo pdf?* 📂';
+  const img = await q.download?.();
+  const url = await uploadImage(img);
+  const docname = text ? text : m.pushName || 'documento';
+  conn.sendFile(m.chat, `http://api.lolhuman.xyz/api/convert/imgtopdf?apikey=${lolkeysapi}&img=${url}`, docname + '.pdf', '', m, false, {asDocument: true});
+};
+handler.command = /^topdf$/i;
+export default handler;
