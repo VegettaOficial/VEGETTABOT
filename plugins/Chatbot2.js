c0951ecfbee74c2fe3374bf2d058c531b1824f83
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-01-26 10:40:42 -0500

    Update Chatbot2.js

--- plugins/Chatbot2.js
+++ plugins/Chatbot2.js
@@ -10,10 +10,7 @@ const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.c
 message: { 
 orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'Super Bot WhatsApp', orderTitle: 'Bang', thumbnail: fs.readFileSync('./media/menus/Menu3.jpg'), sellerJid: '0@s.whatsapp.net'    
 }}}
-const estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) },
-message: { 
-"audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"   
-}}}  
+
 
 if (/^bot$/i.test(m.text)) {
     
