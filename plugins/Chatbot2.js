7d20cbd21c9910fd28347b5fce2e4118bd49623d
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-01-26 10:38:37 -0500

    Update Chatbot2.js

--- plugins/Chatbot2.js
+++ plugins/Chatbot2.js
@@ -3,7 +3,7 @@ let handler = m => m
 
 handler.all = async function (m) {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let vn = './media/bot.mp3'
+
 let bot = `${pickRandom([`Hola soy Elite Bot, en que te puedo ayudar?`, `Aqui estoooy 😜`, `*Hola Aqui estoy yo puedo ayudar?*`,`*Si, dime*`])}
 `.trim()
 const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) },
