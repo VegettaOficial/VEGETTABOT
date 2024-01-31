19e8ffb253b347261681c476c47d1d42d85219fe
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-01-31 16:52:46 -0500

    Update Adv.js

--- plugins/Grupos/Adv.js
+++ plugins/Grupos/Adv.js
@@ -1,35 +1,45 @@
-let handler = async (m, { conn, text, command, usedPrefix }) => {//prems 
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-if (!db.data.chats[m.chat].antiver && m.isGroup) return conn.reply(m.chat, `solo se puede usar si está activada la función:\non antitoxicos`, fkontak, m)
 
-let who 
-let img = 'https://images.vexels.com/media/users/3/153978/isolated/preview/483ef8b10a46e28d02293a31570c8c56-icono-de-trazo-de-color-de-senal-de-advertencia.png'
-if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
-else who = m.chat
-let name = await conn.getName(m.sender)	
-	
-let user = global.db.data.users[who]
-if (!who) return conn.reply(m.chat, `Debes de usar el comando o responder al mensaje de alguien\n${usedPrefix + command} @${name}`, fkontak, m)
-try {
-user.warn += 1
-await m.reply(`${user.warn == 1 ? `@${who.split`@`[0]}` : `@${who.split`@`[0]}`} Recibió una advertencia en este grupo\n\nADVERTENCIA\n⚠️ ${user.warn}/4\n${wm}`, null, { mentions: [who] });
-	
-if (user.warn >= 4) {
-user.warn = 0
-await m.reply(`Te lo advertí varias veces\n@${who.split`@`[0]} ahora serás eliminado(o)`, false, { mentions: [who] })
-user.banned = true
-await conn.groupParticipantsUpdate(m.chat, [who], 'remove') //@${m.sender.split`@`[0]}
-}	
-return !1
-} catch (e) {
-await m.reply(m.chat, `Sucedió un error`, m)
-console.log(`❗ ${usedPrefix + command} ❗❗`)
-console.log(e)	
-}}
-handler.help = ['addprem <@user>']
-handler.tags = ['owner']
-handler.command = /^(advertir|adv)$/i
+let war = global.maxwarn
+let handler = async (m, { conn, text, args, groupMetadata, usedPrefix, command }) => {      
+        let who
+        if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+        else who = m.chat
+        if (!who) throw `Etiqueta o menciona a alguien\n\nEjemplo: ${usedPrefix + command} @user`
+        if (!(who in global.db.data.users)) throw `El usuario no se encuentra en mi base de datos`
+        let name = conn.getName(m.sender)
+        let warn = global.db.data.users[who].warn
+        if (warn < war) {
+            global.db.data.users[who].warn += 1
+            m.reply(`
+*Usuario Advertido* 
+
+*Admin:* ${name}
+*Usuario:* @${who.split`@`[0]}
+*Warns:* ${warn + 1}/${war}
+*Razon:* ${text}`, null, { mentions: [who] }) 
+            m.reply(`
+*ADVERTENCIA*
+Recibiste una advertencia de un admin
+
+*Warns:* ${warn + 1}/${war} 
+Si recibes *${war}* advertencias serás eliminado automáticamente del grupo`, who)
+        } else if (warn == war) {
+            global.db.data.users[who].warn = 0
+            m.reply(`El usuario superó las *${war}* advertencias por lo tanto será eliminado`)
+            await time(3000)
+            await conn.groupParticipantsUpdate(m.chat, [who], 'remove')
+            m.reply(`Fuiste eliminado del grupo *${groupMetadata.subject}* porque ha sido advertido *${war}* veces`, who)
+        }
+}
+handler.help = ['advertencia @user']
+handler.tags = ['group']
+handler.command = ['advertencia','adv'] 
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
+
 export default handler
+
+const time = async (ms) => {
+            return new Promise(resolve => setTimeout(resolve, ms));
+}
