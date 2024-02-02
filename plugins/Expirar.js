60ea108488e749f5d03f625f9d2bbc1185bea674
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-02-02 14:30:01 -0500

    Update Expirar.js

--- plugins/Expirar.js
+++ plugins/Expirar.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     var now = new Date() * 1
     if (now < global.db.data.chats[who].expired) global.db.data.chats[who].expired += nMinutes
     else global.db.data.chats[who].expired = now + nMinutes
-    let teks = `🕔 Se estableció el tiempo de expiración para \n*${await conn.getName(who)}* \n\n*Tiempo* ${args[0]} Minutos\n\n*Cuenta regresiva :* ${msToDate(global.db.data.chats[who].expired - now)}`
+    let teks = `🕔 Se estableció el tiempo de expiración para \n*${await conn.getName(who)}* \n\n» *Tiempo :* ${args[0]} Minutos\n\n*Cuenta regresiva :* ${msToDate(global.db.data.chats[who].expired - now)}`
     conn.reply(m.chat, teks, m)
 }
 
