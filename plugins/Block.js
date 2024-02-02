e9b689228c6c071f09c82decb5611e7caa882a4a
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-02-02 14:15:04 -0500

    Update Block.js

--- plugins/Block.js
+++ plugins/Block.js
@@ -1,44 +1,25 @@
-const handler = async (m, { conn, command }) => {
-  const why = `Uso correcto\n${command} @usuario`;
-  const who = m.mentionedJid[0] || (m.quoted ? m.quoted.sender : null);
-  if (!who) return conn.reply(m.chat, why, m, { mentions: [m.sender] });
+const handler = async (m, { conn }) => {
+  try {
+    const data = await conn.fetchBlocklist();
+    let txt = `*Lista de bloqueados*\n\n*Total :* ${data.length}\n\n\n`;
 
-  let res = [];
-  let groupName;
-
-  switch (command) {
-    case 'blok':
-    case 'block':
-      await conn.updateBlockStatus(who, 'block').then(() => {
-        res.push(who);
-        groupName = m.isGroup ? m.chat : null;
-      });
-      break;
-    case 'unblok':
-    case 'unblock':
-      await conn.updateBlockStatus(who, 'unblock').then(() => {
-        res.push(who);
-        groupName = m.isGroup ? m.chat : null;
-      });
-      break;
-  }
-
-  if (res.length > 0) {
-    let replyText = `*Operación (${command}) completada con ${res.map(v => '@' + v.split('@')[0]).join(', ')}*`;
-    if (groupName) {
-      const group = await conn.getName(groupName);
-      replyText += `\nEn el grupo: ${group}`;
+    for (let i of data) {
+      let blockedUser = global.db.data.blockedUsers && global.db.data.blockedUsers[i] ? global.db.data.blockedUsers[i] : [];
+      let uniqueGroups = [...new Set(blockedUser)]; // Eliminar duplicados
+      let groupNames = await Promise.all(uniqueGroups.map(groupId => conn.getName(groupId)));
+      txt += `@${i.split("@")[0]} - Bloqueado en los grupos: *${groupNames.join(', ')}*\n`;
     }
 
-    conn.reply(m.chat, replyText, m, { mentions: res });
-  } else {
-    conn.reply(m.chat, why, m, { mentions: [m.sender] });
+    conn.reply(m.chat, txt, m, { mentions: await conn.parseMention(txt) });
+  } catch (err) {
+    console.log(err);
+    throw 'No hay números bloqueados';
   }
 };
 
-handler.help = ['block/unblock (@usuario)'];
+handler.help = ['bloqueados'];
 handler.tags = ['owner'];
-handler.command = /^(block|listblock)$/i;
+handler.command = ['bloqueados', 'listblock'];
 handler.rowner = true;
 
 export default handler;
