0bb2057fcde98bc5145bd4beca9f6a2d465f1661
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-02-02 10:56:46 -0500

    Update Expirar.js

--- plugins/Expirar.js
+++ plugins/Expirar.js
@@ -6,10 +6,10 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (m.isGroup) who = args[1] ? args[1] : m.chat
     else who = args[1]
 
-    var nMin = 200 * args[0]
+    var nMinutes = 20000 * args[0]
     var now = new Date() * 1
-    if (now < global.db.data.chats[who].expired) global.db.data.chats[who].expired += nMin
-    else global.db.data.chats[who].expired = now + nMin
+    if (now < global.db.data.chats[who].expired) global.db.data.chats[who].expired += nMinutes
+    else global.db.data.chats[who].expired = now + nMinutes
     let teks = `Se estableció los días de vencimiento para \n*${await conn.getName(who)}* \n\n*Durante:* ${args[0]} Días\n\n*Cuenta regresiva :* ${msToDate(global.db.data.chats[who].expired - now)}`
     conn.reply(m.chat, teks, m)
 }
