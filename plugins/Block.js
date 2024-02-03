98e2a54b43688bcbf5fa5b611de722e287cc3709
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-02-02 21:10:53 -0500

    Update Block.js

--- plugins/Block.js
+++ plugins/Block.js
@@ -7,7 +7,7 @@ const handler = async (m, { conn }) => {
       let blockedUser = global.db.data.blockedUsers && global.db.data.blockedUsers[i] ? global.db.data.blockedUsers[i] : [];
       let uniqueGroups = [...new Set(blockedUser)]; // Eliminar duplicados
       let groupNames = await Promise.all(uniqueGroups.map(groupId => conn.getName(groupId)));
-      txt += `@${i.split("@")[0]} - Bloqueado en los grupos: ${groupNames.join(', ')}\n`;
+      txt += `@${i.split("@")[0]}\n• Grupo: ${groupNames.join(', ')}\n`;
     }
 
     conn.reply(m.chat, txt, m, { mentions: await conn.parseMention(txt) });
