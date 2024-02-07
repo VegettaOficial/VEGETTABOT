73f824662c7f0837c24a2a9026abd2b6f586eaec
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-02-06 21:29:59 -0500

    Update Gh.js

--- plugins/Gh.js
+++ plugins/Gh.js
@@ -1,72 +1,57 @@
 
-let handler = async(m, { conn, text, usedPrefix, command }) => {
-    if (command == 'cambiarbiografia') {
-         if (!text) throw `ingrese un texto`
-           try {
-              await conn.updateProfileStatus(text).catch(_ => _)
-              conn.reply(m.chat, `cambiada con exito`, m)
-      } catch {
-             throw 'error'
-           }
-    }
 
-    if (command == 'cambiarmensaje') {
-        if (!m.quoted) throw 'Responde a el mensaje a editar';
-        if (!text) throw 'Ingrese un texto';
-        if (!m.quoted.isBaileys) throw 'El mensaje no es de el bot';
-      
-        try {
-          await conn.sendMessage(m.chat, {
-            text: text,
-            edit: m.quoted.vM.key,
-          });
-        } catch (e) {
-          try {
-            const edit = m.quoted.sender ? m.message.extendedTextMessage.contextInfo.participant : m.key.participant;
-            const bang = m.quoted.id ? m.message.extendedTextMessage.contextInfo.stanzaId : m.key.id;
-            await conn.sendMessage(m.chat, {
-              text: text,
-              edit: {
-                remoteJid: m.chat,
-                fromMe: false,
-                id: bang,
-                participant: edit,
-              },
-            });
-          } catch (e) {
-            try {
-              await conn.relayMessage(m.chat, {
-                protocolMessage: {
-                  key: m.quoted.vM.key,
-                  type: 14,
-                  editedMessage: {
-                    conversation: text,
-                  },
-                },
-              }, {});
-            } catch (e) {
-              await m.reply(eror);
-            }
-          }
-        }
-    }
-
-    if (command == 'cambiarnombre') {
-        if (!text) throw `ingrese un texto`
-        try {
-          await conn.updateProfileName(text)
-          m.reply('aplicado correctamente')
-        } catch (e) {
-          console.log(e)
-          throw `Error`
-        }
-    }
-  
+let handler = async (m, { conn, isAdmin, isOwner, args, usedPrefix, command }) => {
+  if (!(isAdmin || isOwner)) {
+	  global.dfail('admin', m, conn)
+          throw false
   }
-      
-  handler.help = handler.command = ['cambiarbiografia', 'cambiarmensaje', 'cambiarnombre']
-  handler.tags = ['owner']
-  
-  handler.owner = true
-  
-  export default handler
+  let isClose = {
+	  'open': 'not_announcement',
+	  'buka': 'not_announcement',
+      'on': 'not_announcement',
+	  '1': 'not_announcement',
+	  'close': 'announcement',
+	  'tutup': 'announcement',
+      'off': 'announcement',
+      '0': 'announcement',
+  }[(args[0] || '')]
+  if (isClose === undefined) {
+	  let caption = `
+*FORMATO ERRONEO!!*
+ 
+ ${usedPrefix + command} open 1*
+ ${usedPrefix + command} close 1*
+ *Ejemplo de uso:* *${usedPrefix + command} close 1* 
+ Para que el grupo este cerrado una hora.*
+
+`
+      m.reply(caption)
+	  throw false
+  }
+  let timeoutset = 86400000 * args[1] / 24
+  await conn.groupSettingUpdate(m.chat, isClose).then(async _=> {
+	  m.reply(` *Grupo ${isClose == 'announcement' ? 'cerrado' : 'abierto'} ${args[1] ? `durante*${clockString(timeoutset)}*` : ''}`)
+  })
+  if (args[1]) {
+	 setTimeout(async () => {
+            await conn.groupSettingUpdate(m.chat, `${isClose == 'announcement' ? 'not_announcement' : 'announcement'}`).then(async _=>{
+		    conn.reply(m.chat, `${isClose == 'not_announcement' ? '*El grupo ha sido cerrado, ahora solo los administradores pueden enviar mensajes!*' : '*El grupo se ha abierto, ahora todos los miembros pueden enviar mensajes!*'}!`)
+	    })
+        }, timeoutset)
+  }
+  }
+handler.help = ['grouptime <open/close> <horas>']
+handler.tags = ['group']
+handler.command = /^(grouptime|gctime)$/i
+
+handler.botAdmin = true
+handler.group = true 
+export default handler
+
+function clockString(ms) {
+  let h = Math.floor(ms / 3600000)
+  let m = Math.floor(ms / 60000) % 60
+  let s = Math.floor(ms / 1000) % 60
+  console.log({ms,h,m,s})
+  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
+                                                      }
