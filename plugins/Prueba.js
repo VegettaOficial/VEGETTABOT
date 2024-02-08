edb241280f1db7a30a659848363841dfe8a90ae2
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-02-08 14:03:57 -0500

    Update Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -1,23 +1,28 @@
-import PhoneNumber from 'awesome-phonenumber'
-let handler = async (m, { conn, __dirname, isRowner, isOwner, isBotAdmin, usedPrefix, groupMetadata, groups, _package, participants }) => { 
-let blockedUser = global.db.data.blockedUsers && global.db.data.blockedUsers[i] ? global.db.data.blockedUsers[i] : [];
-const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
-}
+let handler = async (m, { conn, isAdmin, isOwner, args, usedPrefix, command }) => {
+  if (!(isAdmin || isOwner)) {
+    global.dfail('admin', m, conn);
+    throw false;
+  }
 
-let txt
-const chats = Object.entries(conn.chats).filter(([jid, data]) => jid && data.isChats)
-groups = Object.values(await conn.groupFetchAllParticipating())
-txt = `ESTOY EN ESTOS GRUPOS ✅\n`
-for (let i = 0; i < groups.length; i++) {
-txt += ` 
-*GRUPO*: ${groups[i].subject}
-*ID:* ${groups[i].id}\n`
-}
-m.reply(txt.trim())
-}
-handler.help = ['groups', 'grouplist']
-handler.tags = ['info']
-handler.command = /^(grupos)$/i
-handler.rowner = true;
-handler.exp = 30
-export default handler
+  if (args[0] === undefined || isNaN(args[0])) {
+    m.reply('*FORMATO ERRONEO!!*\n\n*Ejemplo de uso:* *' + usedPrefix + command + ' 1*\n*Para cerrar el grupo durante una hora.*');
+    throw false;
+  }
+
+  let timeoutset = 86400000 * args[0] / 24;
+  await conn.groupSettingUpdate(m.chat, 'announcement');
+  m.reply(`*Grupo cerrado durante ${args[0]} horas*`);
+
+  setTimeout(async () => {
+    await conn.groupSettingUpdate(m.chat, 'not_announcement');
+    conn.reply(m.chat, '*El grupo se ha abierto, ahora todos los miembros pueden enviar mensajes!*');
+  }, timeoutset);
+};
+
+handler.help = ['abrirgrupoen <horas>'];
+handler.tags = ['group'];
+handler.command = /^(abrirgrupoen)$/i;
+handler.botAdmin = true;
+handler.group = true;
+
+export default handler;
