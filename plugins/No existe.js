c65d77d98ea591e4567bb1b96f4848c0d1960483
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-02-09 09:08:45 -0500

    Delete plugins/No existe.js

--- plugins/No existe.js
+++
@@ -1,7 +0,0 @@
-let handler = async (m, { conn, command }) => {
-  conn.sendMessage(m.chat, `El comando '${command}' no existe, @${m.sender.split("@")[0]}. Puedes usar el comando .help para ver la lista de comandos disponibles.`, 'text', { quoted: m })
-}
-
-handler.command = /^(.*)$/i
-
-export default handler
