420a8eff75e13c883a138fea6ec974fa28d56c05
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-02-08 21:08:44 -0500

    Update No existe.js

--- plugins/No existe.js
+++ plugins/No existe.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, command }) => {
-  conn.reply(m.chat, `El comando '${command}' no existe. Puedes usar el comando .menu para ver la lista de comandos disponibles.`, m)
+  conn.reply(m.chat, `El comando '${command}' no existe. Puedes usar el comando .help para ver la lista de comandos disponibles.`, m)
 }
 
-handler.all = /^(.*)$/i
+handler.command = /^(.*)$/i
 
 export default handler
