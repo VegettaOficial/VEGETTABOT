12ef12fa5d9ccf529afcb1524db17ad0ee086faf
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-02-10 19:29:04 -0500

    Update encuesta.js

--- plugins/encuesta.js
+++ plugins/encuesta.js
@@ -7,8 +7,8 @@ let handler = async (m, {
 }) => {
     // Split the message text using the '|' character and slice the array to remove the first element.
     let a = text.split("|").slice(1)
-    if (!a[1]) throw "Format\n" + usedPrefix + command + " hello |yes|no"
-    if (a[12]) throw "Too many options, Format\n" + usedPrefix + command + " hello |yes|no"
+    if (!a[1]) throw "Ingresa el texto\n" + usedPrefix + command + " hello |si|no"
+    if (a[12]) throw "Too many options, Format\n" + usedPrefix + command + " hello |si|no"
     // Check for duplicate options in the poll.
     if (checkDuplicate(a)) throw "Duplicate options in the message!"
     let cap = "*Polling Request By* " + m.name + "\n*Message:* " + text.split("|")[0]
