2182f4126664b55c0304bef38c2e75ce83010262
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-02-10 19:41:40 -0500

    Update encuesta.js

--- plugins/encuesta.js
+++ plugins/encuesta.js
@@ -6,12 +6,12 @@ let handler = async (m, {
     command
 }) => {
     // Split the message text using the '|' character and slice the array to remove the first element.
-    
-    if (!a[1]) throw "Ingresa el texto\n" + usedPrefix + command + " hello |confirmo|desconfirmo"
-    if (a[12]) throw "Too many options, Format\n" + usedPrefix + command + " hello |confirmo|desconfirmo"
+    let a = text.split("|").slice(1)
+    if (!a[1]) throw "Format\n" + usedPrefix + command + " hello |yes|no"
+    if (a[12]) throw "Too many options, Format\n" + usedPrefix + command + " hello |yes|no"
     // Check for duplicate options in the poll.
     if (checkDuplicate(a)) throw "Duplicate options in the message!"
-    let cap = "*ENCUESTA REALIZADA POR:* " + m.name + "\n*Mensaje:* "
+    let cap = "*Polling Request By* " + m.name + "\n*Message:* " + text.split("|")[0]
 
    
     const pollMessage = {
@@ -26,15 +26,9 @@ let handler = async (m, {
     })
 }
 
-//handler.help = ["poll question|option|option"]
-//handler.tags = ["group"]
-//handler.command = /^encues(l((l?ing|ls)|l)|ta?)$/i
-
-handler.help = ['encuesta <Horas>']
-handler.tags = ['owner']
-handler.command = /^(encuesta|encuestas)$/i
-export default handler
-
+handler.help = ["poll question|option|option"]
+handler.tags = ["group"]
+handler.command = /^(encuesta)$/i
 
 export default handler
 
