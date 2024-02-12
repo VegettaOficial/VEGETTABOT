5d84f01ebb7b954bac1137021043168667b553ae
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-02-12 15:02:19 -0500

    Update encuesta.js

--- plugins/encuesta.js
+++ plugins/encuesta.js
@@ -1,38 +1,30 @@
+
 let handler = async (m, {
     conn,
     text,
     args,
-    usedPrefix,
     command
 }) => {
-    // Split the message text using the '|' character and slice the array to remove the first element.
-    let a = text.split("|").slice(1)
-    if (!a[1]) throw "Ejemplo de uso\n" + usedPrefix + command + " Quieren pizza|si|no"
-    if (a[12]) throw "Ejemplo de uso\n" + usedPrefix + command + " Quieren pizza|si|no"
-    // Check for duplicate options in the poll.
-    if (checkDuplicate(a)) throw "Duplicate options in the message!"
-    let cap = "*Encuesta hecha por:* " + m.name + "\n*Mensaje:* " + text.split("|")[0]
+    let question = text.replace(/:.*/,'').trim()
+    if (!question) {
+        throw "Por favor, haz una pregunta para la encuesta"
+    }
 
-   
     const pollMessage = {
-        name: cap,
-        values: a,
+        name: question,
+        values: ["Sí", "No"],
         multiselect: false,
         selectableCount: 1
     }
-  
+
     await conn.sendMessage(m.chat, {
         poll: pollMessage
     })
 }
 
-handler.help = ["poll question |option|option"]
-handler.tags = ["group"]
-handler.command = /^(encuesta)$/i
+handler.help = ['encuesta <pregunta>']
+handler.tags = ['group'] 
+handler.command = ['poll', 'encuesta', 'polling'] 
+handler.group = true
 
 export default handler
-
-// Function to check for duplicate elements in an array.
-function checkDuplicate(arr) {
-    return new Set(arr).size !== arr.length
-}
