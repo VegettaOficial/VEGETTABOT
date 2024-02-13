6727a9a6af8d735be682a907d547145a697aeb98
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-02-13 12:35:49 -0500

    Create encuesta.js

---
+++ plugins/Grupos/encuesta.js
@@ -0,0 +1,32 @@
+let handler = async (m, {
+    conn,
+    text,
+    args,
+    command,
+    sender
+}) => {
+
+    let question = text.replace(/:.*/,'').trim()
+    if (!question) {
+        throw "Por favor, haz una pregunta para la encuesta"
+    }
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let cap = `*Encuesta hecha por: *${m.name}*\n*Mensaje:* ${text}`
+    const pollMessage = {
+        name: cap,
+        values: ["Confirmo", "Negado"],
+        multiselect: false,
+        selectableCount: 1
+    }
+
+    await conn.sendMessage(m.chat, {
+        poll: pollMessage
+    })
+}
+
+handler.help = ['encuesta <pregunta>']
+handler.tags = ['group'] 
+handler.command = ['poll', 'encuesta', 'polling'] 
+handler.group = true
+
+export default handler
