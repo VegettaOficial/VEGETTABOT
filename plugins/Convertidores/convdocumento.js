d404ca3b1142edef5615b0278f27e96ba3d9fd7d
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-02-24 22:39:46 -0500

    Update and rename png.js to convdocumento.js

--- plugins/Convertidores/convdocumento.js
+++ plugins/Convertidores/convdocumento.js
@@ -25,6 +25,6 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
 handler.help = ['convdocumento <audio/video>']
 handler.tags = ['tools']
-handler.command = ['convdocumento']
+handler.command = ['documento']
 
 export default handler
