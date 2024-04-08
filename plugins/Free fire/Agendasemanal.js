87eb8480deeb635d9e42a075fc7e384e0b2addda
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-04-08 11:20:36 -0500

    Update Agendasemanal.js

--- plugins/Free fire/Agendasemanal.js
+++ plugins/Free fire/Agendasemanal.js
@@ -2,7 +2,7 @@ let handler = async (m, { isPrems, conn }) => {
 let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
 if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
 
-let img = 'https://telegra.ph/file/17ffe819abcd884409f3e.jpg' 
+let img = 'https://telegra.ph/file/daf0bc0fc3c1e4c471c5c.jpg' 
 let texto = `📕 ¡LA #AGENDASEMANAL ESTÁ AQUÍ! 📕
 
 Elegancia, poder y #Potencia están por llegar. 👇
