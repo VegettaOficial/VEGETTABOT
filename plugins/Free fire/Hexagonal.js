10e58d4bb658422afcae9dc924a64a4713b745b6
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-04-11 12:29:29 -0500

    Create Hexagonal.js

---
+++ plugins/Free fire/Hexagonal.js
@@ -0,0 +1,50 @@
+let handler = async (m, { isPrems, conn }) => {
+let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
+if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
+
+let img = 'https://telegra.ph/file/46eedb522f1676bd759a7.png' 
+let texto = `*HEXAGONAL*
+⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨
+
+⏰┨𝐇𝐎𝐑𝐀: 
+👕┨𝐕𝐄𝐒𝐓𝐈𝐌𝐄𝐍𝐓𝐀: 
+🛟┨𝐂𝐀𝐒𝐈𝐋𝐋𝐀𝐒:
+
+_*Escuadra 1:*_ 
+➢⚜️
+➢⚔️
+➢⚔️
+➢⚔️
+
+_*Escuadra 2:*_ 
+➢⚜️ 
+➢⚔️
+➢⚔️
+➢⚔️
+
+𝙎𝙐𝙋𝙇𝙀𝙉𝙏𝙀𝙎 *Igual estar a la hora del vs en DC*:
+
+➠
+➠
+➠`
+
+const fkontak = {
+	"key": {
+    "participants":"0@s.whatsapp.net",
+		"remoteJid": "status@broadcast",
+		"fromMe": false,
+		"id": "Halo"
+	},
+	"message": {
+		"contactMessage": {
+			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+		}
+	},
+	"participant": "0@s.whatsapp.net"
+}
+await conn.sendFile(m.chat, img, 'img.jpg', texto, fkontak)
+global.db.data.users[m.sender].lastcofre = new Date * 1
+}
+handler.command = ['hexagonal'] 
+handler.register = false 
+export default handler
