4ec849f57dd3258c49b7b50a010d449a0566324b
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-04-12 18:32:50 -0500

    Update Logo.js

--- plugins/Logo.js
+++ plugins/Logo.js
@@ -1,18 +1,11 @@
+var handler = async (m, {conn}) => {
 
-const handler = async (m, {conn}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.maker_gay
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/gay', { avatar: await conn.profilePictureUrl(who, "image").catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'), }), 'error.png', '*MIREN A ESTE GAY 😂🏳️‍🌈*', m)
 
-  const vn = './media/gay2.mp3';
-  const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-  await conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/gay', {
-    avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-  }), 'error.png', tradutor.texto1, m);
-  await conn.sendMessage(m.chat, {audio: {url: vn}, fileName: `error.mp3`, mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
-};
-handler.help = ['gay'];
-handler.tags = ['maker'];
-handler.command = /^(gay)$/i;
-export default handler;
+}
+handler.help = ['gay']
+handler.tags = ['juegos']
+handler.command = /^(gayy)$/i
+
+export default handler
