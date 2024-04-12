2103793d1e28a6621ac1752610ff59fc41d6e27f
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-04-12 18:30:16 -0500

    Update Logo.js

--- plugins/Logo.js
+++ plugins/Logo.js
@@ -1,13 +1,18 @@
-let handler = async (m, { conn }) => {
-  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  let name = conn.getName(who)
-  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/gay', {
-    avatar: await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png'), 
-  }), 'gay.png', `*Gay:* ${name}\n\nQuién quiere violar a este gay? `, m)
-}
 
-handler.help = ['gay @user']
-handler.tags = ['fun']
-handler.command = ['gayy'] 
+const handler = async (m, {conn}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.maker_gay
 
-export default handler
+  const vn = './media/gay2.mp3';
+  const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+  await conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/gay', {
+    avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+  }), 'error.png', tradutor.texto1, m);
+  await conn.sendMessage(m.chat, {audio: {url: vn}, fileName: `error.mp3`, mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
+};
+handler.help = ['gay'];
+handler.tags = ['maker'];
+handler.command = /^(gay)$/i;
+export default handler;
