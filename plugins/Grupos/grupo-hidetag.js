e568473557050b2b805b517f02826c58a52374e7
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-04-14 20:33:32 -0500

    Update grupo-hidetag.js

--- plugins/Grupos/grupo-hidetag.js
+++ plugins/Grupos/grupo-hidetag.js
@@ -63,7 +63,7 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
     }
   }
 };
-handler.command = /^(hidetag|notificar|notify)$/i;
+handler.command = /^(hidetag|notificar|notify|n|noti|avisos|aviso)$/i;
 handler.group = true;
 handler.admin = true;
 export default handler;
