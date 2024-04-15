645ec5146c5a5250463d2057faa1007ed284f007
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-04-15 17:54:39 -0500

    Update Logo.js

--- plugins/Logo.js
+++ plugins/Logo.js
@@ -9,5 +9,8 @@ export default handler
 global.lolivid = [
 "https://telegra.ph/file/7266c4a284d0a2df54589.mp4",
 "https://telegra.ph/file/40c07f641420a80b0f8cf.mp4",
+  "https://telegra.ph/file/daa050ed7baa88ac2d4b8.mp4",
+  "https://telegra.ph/file/ed3117a8ed94cfa96f9b2.mp4",
+"https://telegra.ph/file/cd493e2a01bc573f94638.mp4",
 "https://telegra.ph/file/79816642cc73eb500d2fe.mp4"
 ]
