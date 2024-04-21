e3664786ac3e24feecd9ab008e9a1c219cd0a4fd
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-04-21 15:42:59 -0500

    Create Tiktokrandom.js

---
+++ plugins/Tiktok/Tiktokrandom.js
@@ -0,0 +1,14 @@
+let handler  = async (m, { conn, usedPrefix, command }) => {
+let res = await tiktokfrases[Math.floor(Math.random() * tiktokfrases.length)]
+await m.react('⛱️')
+conn.sendMessage(m.chat, { video: { url: res }, caption: `» 𝙁𝙍𝘼𝙎𝙀𝙎 𝙏𝙄𝙆 𝙏𝙊𝙆 ⛱️` }, { quoted: m })}
+handler.help = ['tiktokfrases']
+handler.tags = ['random']
+handler.command = /^(tiktokrandom)$/i
+export default handler
+global.tiktokfrases = [
+"https://telegra.ph/file/84cf4ac7e73ea9d878180.mp4",
+"https://telegra.ph/file/e31c8417bd6d58a0d5991.mp4",
+  "https://telegra.ph/file/c8615d243f5c2bace002a.mp4",
+"https://telegra.ph/file/5c62ae6dcc0262cb7638e.mp4"
+]
