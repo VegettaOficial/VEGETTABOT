0fd2b6697ed1525ed331884accf09b90035886f2
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-05-05 10:19:08 -0500

    Update Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -10,6 +10,9 @@ while (b === a)
 m.reply(`*${toM(a)},* _Te toco donar una sala mi pana juegas y juegas y no te he visto colaborar una sala._`, null, {
 mentions: [a, b]
 })}
+await conn.sendFile(m.chat, img, 'img.jpg', texto, fkontak)
+global.db.data.users[m.sender].lastcofre = new Date * 1
+}
 handler.help = ['formarpareja']
 handler.tags = ['main', 'fun']
 handler.command = ['pepe']
