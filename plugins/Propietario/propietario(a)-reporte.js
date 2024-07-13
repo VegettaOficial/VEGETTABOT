c203dfe4d47e5e7a7854b27587320f6ad7bb3b2c
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-07-13 14:08:43 -0500

    Actualizar propietario(a)-reporte.js

--- plugins/Propietario/propietario(a)-reporte.js
+++ plugins/Propietario/propietario(a)-reporte.js
@@ -7,7 +7,7 @@ let teks = `*╭━━[ 𝙍𝙀𝙋𝙊𝙍𝙏𝙀 | 𝙍𝙀𝙋𝙊𝙍𝙏 
 //contextInfo: {
 //mentionedJid: [m.sender]
 //}})
-conn.reply('593993370003@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {
+conn.reply('593992402778@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {
 contextInfo: {
 mentionedJid: [m.sender]
 }})
