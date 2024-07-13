a7f036760be1ed2464bd26506ef84e9a6b7d29ed
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-07-13 14:08:21 -0500

    Actualizar grupo-kick.js

--- plugins/Grupos/grupo-kick.js
+++ plugins/Grupos/grupo-kick.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
-if (!global.db.data.settings[conn.user.jid].restrict) throw '*[ ⚠️ ] MI CREADOR TIENE DESACTIVADO ESTA FUNCIÓN, CONTÁCTATE CON EL A SU PRIVADO PARA QUE TE AYUDÉ.*\n\ 💻 +593993370003'
+if (!global.db.data.settings[conn.user.jid].restrict) throw '*[ ⚠️ ] MI CREADOR TIENE DESACTIVADO ESTA FUNCIÓN, CONTÁCTATE CON EL A SU PRIVADO PARA QUE TE AYUDÉ.*\n\ 💻 +593992402778'
 let kicktext = `⚠️ *ETIQUETA A LA PERSONA O RESPONDE SU MENSAJE PARA ELIMINARLO DE ESTE GRUPO.*`
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kicktext, m.chat, { mentions: conn.parseMention(kicktext)}) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
