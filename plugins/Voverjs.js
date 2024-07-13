abc80ee851c545b163016cadcbc79a5a7eaec195
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-07-13 11:29:57 -0500

    Update Voverjs.js

--- plugins/Voverjs.js
+++ plugins/Voverjs.js
@@ -1,35 +1,64 @@
+/* Creado/adaptado por Bruno Sobrino (https://github.com/BrunoSobrino) */
+
 import fetch from 'node-fetch';
-
-
-
-const handler = async (m, {conn, text}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.downloader_soundcloud
-
-
-  if (!text) throw `${tradutor.texto1}`;
+import axios from 'axios';
+import {load} from 'cheerio';
+const handler = async (m, {text, usedPrefix, command, conn}) => {
+if (!text) throw`️${lenguajeGB['smsAvisoMG']()}${mid.smsMalused7}\n${usedPrefix + command} El Gato con botas`
+  let aaaa;
+  let img;
   try {
-    const res = await fetch(`https://api-v2.soundcloud.com/search/tracks?q=${text}&client_id=iZIs9mchVcX5lhVRyQGGAYlNPVldzAoX`);
-    const json2 = await res.json();
-    let permalinkUrl;
-    if (json2.collection.length > 0) {
-      const randomIndex = Math.floor(Math.random() * json2.collection.length);
-      const randomObject = json2.collection[randomIndex];
-      permalinkUrl = randomObject.permalink_url;
-    } else {
-      permalinkUrl = await json2.collection[0].permalink_url;
-    }
-    const res2 = await fetch(`https://api.akuari.my.id/downloader/scdl?link=${permalinkUrl}`);
-    const json = await res2.json();
-    const shortUrl = await (await fetch(`https://tinyurl.com/api-create.php?url=${json.link}`)).text();
-    const soundcloudt = `${tradutor.texto2[0]} ${json.title}\n┴\n┬\n${tradutor.texto2[1]} ${shortUrl}\n┴\n┬\n${tradutor.texto2[2]}\n├‣ _﹫ᴛʜᴇ ᴍʏsᴛɪᴄ ﹣ ʙᴏᴛ_\n┴`;
-    await conn.sendFile(m.chat, json.thumb, '', soundcloudt, m);
-    await conn.sendMessage(m.chat, {audio: {url: json.link}, fileName: `${json.title}.mp3`, mimetype: 'audio/mpeg'}, {quoted: m});
+    aaaa = await searchC(text);
+    img = 'https://cinefilosoficial.com/wp-content/uploads/2021/07/cuevana.jpg';
   } catch {
-    throw `${tradutor.texto3}`;
+    aaaa = await searchP(text);
+    img = 'https://elcomercio.pe/resizer/RJM30xnujgfmaODGytH1rRVOrAA=/400x0/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/BJ2L67XNRRGHTFPKPDOEQ2AH5Y.jpg';
+  }
+  if (aaaa == '') throw `️${lenguajeGB['smsAvisoFG']()}${mid.buscador10}`
+  const res = await aaaa.map((v) => `*🎬 • ${mid.smsYT1}:* ${v.title}\n*🍿 • ${mid.smsYT4}:* ${v.link}`).join`\n\n───────────────\n\n`;
+  const ads = `*💫 • ${mid.buscador11}*\nhttps://block-this.com/block-this-latest.apk\n\n≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣\n\n`
+  conn.sendMessage(m.chat, {image: {url: img}, caption: ads + res}, {quoted: m});
+};
+handler.command = ['cuevana', 'pelisplus'];
+handler.level = 2
+handler.register = true
+export default handler;
+
+const safeLoad = async (url, options = {}) => {
+  try {
+    const {data: pageData} = await axios.get(url, options);
+    const $ = load(pageData);
+    return $;
+  } catch (err) {
+    if (err.response) {
+      throw new Error(err.response.statusText);
+    }
+    throw err;
   }
 };
-handler.command = /^(soundcloud|coverr)$/i;
-export default handler;
+
+async function searchC(query, numberPage = 1) {
+  const $ = await safeLoad(`https://cuevana3.mu/page/${numberPage}/`, {
+    params: {s: query}});
+  const resultSearch = [];
+  $('.results-post > article').each((_, e) => {
+    const element = $(e);
+    const title = element.find('header > h2').text();
+    const link = element.find('.lnk-blk').attr('href');
+    resultSearch.push({title: title, link: link});
+  });
+  return resultSearch;
+}
+
+async function searchP(query, numberPage = 1) {
+  const $ = await safeLoad(`https://pelisplushd.cx/search/`, {
+    params: {s: query, page: numberPage}});
+  const resultSearch = [];
+  $('a[class^=\'Posters\']').each((_, e) => {
+    const element = $(e);
+    const title = element.find('.listing-content > p').text();
+    const link = element.attr('href');
+    resultSearch.push({title: title, link: link});
+  });
+  return resultSearch;
+}
